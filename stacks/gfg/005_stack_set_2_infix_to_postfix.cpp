75dd4c11f302e848419d19f4b6608c5be05a9ccc
Artem Pushkin <idfumg@gmail.com>
2020-11-15 13:08:13 +0300

    Optimize Infix to Postfix

--- stacks/gfg/005_stack_set_2_infix_to_postfix.cpp
+++ stacks/gfg/005_stack_set_2_infix_to_postfix.cpp
@@ -1,5 +1,9 @@
 #include "../../template.hpp"
 
+bool isoperator(char ch) {
+    return ch == '*' or ch == '-' or ch == '+' or ch == '^' or ch == '/';
+}
+
 int prec(char ch) {
     if (ch == '^') {
         return 3;
@@ -21,7 +25,7 @@ string InfixToPostfix(const string& s) {
     int n = s.size();
 
     for (int i = 0; i < n; ++i) {
-        if ((s[i] >= 'a' and s[i] <= 'z') or (s[i] >= 'A' and s[i] <= 'Z')) {
+        if (isalpha(s[i]) or isdigit(s[i])) {
             ans += s[i];
         }
         else if (s[i] == '(') {
@@ -31,11 +35,9 @@ string InfixToPostfix(const string& s) {
             while (not st.empty() and st.top() != '(') {
                 ans += st.top(); st.pop();
             }
-            if (st.top() == '(') {
-                st.pop();
-            }
+            st.pop();
         }
-        else {
+        else if (isoperator(s[i])) {
             while (not st.empty() and prec(s[i]) <= prec(st.top())) {
                 ans += st.top(); st.pop();
             }
@@ -51,6 +53,5 @@ string InfixToPostfix(const string& s) {
 }
 
 int main() { TimeMeasure _;
-    string exp = "a+b*(c^d-e)^(f+g*h)-i";
-    cout << InfixToPostfix(exp) << '\n'; // abcd^e-fgh*+^*+i-
+    cout << InfixToPostfix("a+b*(c^d-e)^(f+g*h)-i") << '\n'; // abcd^e-fgh*+^*+i-
 }
