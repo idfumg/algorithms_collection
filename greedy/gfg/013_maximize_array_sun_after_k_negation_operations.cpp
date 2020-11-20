c2ea70ced654862dd68e6dc0a203b06b11e432a0
Artem Pushkin <idfumg@gmail.com>
2020-11-20 22:27:28 +0300

    Update Maximize array sum after k negations

--- greedy/gfg/013_maximize_array_sun_after_k_negation_operations.cpp
+++ greedy/gfg/013_maximize_array_sun_after_k_negation_operations.cpp
@@ -1,21 +1,32 @@
 #include "../../template.hpp"
 
 int MaximizeArraySum(vi arr, int k) {
-    sort(arr);
     int n = arr.size();
     int ans = 0;
-    for (int i = 0; i < n; ++i) {
-        if (arr[i] < 0 and k > 0) {
-            arr[i] *= -1;
-            --k;
-        }
-        else if (k > 0) {
-            arr[i] *= k & 1 ? -1 : 1;
-            k = 0;
-        }
-        ans += arr[i];
+    sort(arr.begin(), arr.end());
+    int i = 0;
+    int lastNegative = -1;
+    while (i < n and arr[i] < 0) {
+        arr[i] *= -1;
+        ++i;
+        --k;
+        lastNegative = i;
     }
-    return ans;
+    if (arr[i] == 0) {
+        k = 0;
+    }
+    else if (k % 2 == 0) {
+        k = 0;
+    }
+    else {
+        if (lastNegative != -1 and arr[lastNegative] < arr[i]) {
+            arr[lastNegative] *= -1;
+        }
+        else {
+            arr[i] *= -1;
+        }
+    }
+    return accumulate(arr.begin(), arr.end(), 0);
 }
 
 int main() { TimeMeasure _;
