f18771363c4a6f9a5ea90e5e9a187caf56d5baee
Artem Pushkin <idfumg@gmail.com>
2020-11-20 22:26:16 +0300

    Update Arrange numbers to make the biggest number

--- arrays/gfg/019_given_an_array_of_numbers_arrange_the_numbers_to_form_the_biggest_number.cpp
+++ arrays/gfg/019_given_an_array_of_numbers_arrange_the_numbers_to_form_the_biggest_number.cpp
@@ -1,24 +1,20 @@
 #include "../../template.hpp"
 
-bool pred(const string& b, const string& a) {
-    const string x = b + a;
-    const string y = a + b;
-    return x < y;
+bool pred(int a, int b) {
+    const string x = to_string(a);
+    const string y = to_string(b);
+    return stoi(x + y) > stoi(y + x);
 }
 
-void FormABigestNumber(vi arr_) {
-    int n = arr_.size();
-    vector<string> arr(n);
-    transform(arr_.begin(), arr_.end(), arr.begin(), [&](int a) {return to_string(a);});
-    sort(arr.rbegin(), arr.rend(), pred);
-    for_each(arr.begin(), arr.end(), [](const auto& s){cout << s;});
-    cout << '\n';
+void FormABigestNumber(vi arr) {
+    sort(arr.begin(), arr.end(), pred);
+    debugn(arr);
 }
 
 int main() { TimeMeasure _;
     vi arr1 = {54, 546, 548, 60};
     vi arr2 = {1, 34, 3, 98, 9, 76, 45, 4};
 
-    FormABigestNumber(arr1);
-    FormABigestNumber(arr2);
+    FormABigestNumber(arr1); // 6054854654
+    FormABigestNumber(arr2); // 998764543431
 }
