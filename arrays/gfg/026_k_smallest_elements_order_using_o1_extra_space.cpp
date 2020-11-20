4a20456cbcce02f26e0abe7b5143912da92af467
Artem Pushkin <idfumg@gmail.com>
2020-11-21 00:03:22 +0300

    Update K smallest elements in an array problem

--- arrays/gfg/026_k_smallest_elements_order_using_o1_extra_space.cpp
+++ arrays/gfg/026_k_smallest_elements_order_using_o1_extra_space.cpp
@@ -2,47 +2,63 @@
 
 void KSmallestElems(vi arr, int k) {
     int n = arr.size();
-    unordered_map<int, int> position;
-    for (int i = 0; i < n; ++i) {
-        position[arr[i]] = i;
+    stable_sort(arr.begin(), arr.end());
+    for (int i = 0; i < k and i < n; ++i) {
+        cout << arr[i] << ' ';
     }
-    sort(arr.begin(), arr.end());
-    sort(arr.begin(), arr.begin() + k, [&](int a, int b){return position[a] < position[b];});
-    for_each(arr.begin(), arr.begin() + k, [](int v){cout << v << ' ';});
-    cout << '\n';
+    cout << endl;
 }
 
-void KSmallestElems2(vi arr, int k) { // with destroying non-related array values
+void KSmallestElems2(vi arr, int k) {
     int n = arr.size();
-
-    for (int i = k; i < n; ++i) {
-        int maxi = -INF, maxpos = INF;
-        for (int j = 0; j < k; ++j) {
-            if (arr[j] > maxi) {
-                maxi = arr[j];
-                maxpos = j;
+    for (int i = 0; i < k and i < n; ++i) {
+        int idx = i;
+        for (int j = i + 1; j < n; ++j) {
+            if (arr[j] < arr[idx]) {
+                idx = j;
             }
         }
-
-        if (arr[i] < maxi) {
-            arr[maxpos] = arr[i];
-            arr[i] = INF;
+        if (idx != i) {
+            swap(arr[i], arr[idx]);
         }
     }
-
     for (int i = 0; i < k; ++i) {
         cout << arr[i] << ' ';
     }
-    cout << '\n';
+    cout << endl;
+}
+
+void KSmallestElems3(vi arr, int k) {
+    int n = arr.size();
+    for (int i = k; i < n; ++i) {
+        int maxvalue = arr[0];
+        int maxpos = 0;
+        for (int j = 1; j < k; ++j) {
+            if (arr[j] > maxvalue) {
+                maxvalue = arr[j];
+                maxpos = j;
+            }
+        }
+        if (maxvalue > arr[i]) {
+            arr[maxpos] = arr[i];
+        }
+    }
+    for (int i = 0; i < k; ++i) {
+        cout << arr[i] << ' ';
+    }
+    cout << endl;
 }
 
 int main() { TimeMeasure _;
-    vi arr1 = {4, 2, 6, 1, 5}; int k1 = 3;
-    vi arr2 = {4, 12, 16, 21, 25}; int k2 = 3;
+    vi arr1 = {4, 2, 6, 1, 5}; int k1 = 3; // 4 2 1
+    vi arr2 = {4, 12, 16, 21, 25}; int k2 = 3; // 16 12 4
 
     KSmallestElems(arr1, k1); // O(nlogn) + Om(n)
     KSmallestElems(arr2, k2);
     cout << '\n';
-    KSmallestElems2(arr1, k1); // O(n) + Om(1)
+    KSmallestElems2(arr1, k1); // O(n*k) + Om(1)
     KSmallestElems2(arr2, k2);
+    cout << '\n';
+    KSmallestElems3(arr1, k1); // O(n) + Om(1)
+    KSmallestElems3(arr2, k2);
 }
