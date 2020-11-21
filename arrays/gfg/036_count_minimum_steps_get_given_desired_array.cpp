f6ba4dd31b318c39863c5a5e9b47759470c60750
Artem Pushkin <idfumg@gmail.com>
2020-11-21 17:31:22 +0300

    Update the Min steps to desired array problem

--- arrays/gfg/036_count_minimum_steps_get_given_desired_array.cpp
+++ arrays/gfg/036_count_minimum_steps_get_given_desired_array.cpp
@@ -1,35 +1,29 @@
 #include "../../template.hpp"
 
 int CountMinSteps(vi arr) {
+    int mini = *min_element(arr.begin(), arr.end());
+    if (mini == 1) {
+        return accumulate(arr.begin(), arr.end(), 0);
+    }
     int n = arr.size();
-    int ans = 0;
-    int doubleCount = INF;
+    int ans = n + log2(mini);
     for (int i = 0; i < n; ++i) {
-        if (arr[i] <= 1) {
-            doubleCount = 0;
-            break;
-        }
-
-        if (arr[i] & 1) {
-            doubleCount = min(doubleCount, (int)log2(arr[i] - 1));
-        }
-        else {
-            doubleCount = min(doubleCount, (int)log2(arr[i]));
-        }
+        ans += arr[i] - (mini & 1 ? mini - 1 : mini);
     }
+    return ans;
+}
 
-    for (int i = 0; i < n; ++i) {
-        if (doubleCount == 0) {
-            ans += arr[i];
-            continue;
-        }
-
-        if (arr[i] & 1) {
-            ans += arr[i] - pow(2, doubleCount);
-        }
-        ans += 1;
+int CountMinSteps2(vi arr) {
+    int mini = *min_element(arr.begin(), arr.end());
+    int sum = accumulate(arr.begin(), arr.end(), 0);
+    if (mini == 1) {
+        return sum;
     }
-    return ans + doubleCount;
+    return
+        arr.size() +
+        log2(mini) +
+        sum -
+        (mini & 1 ? mini - 1 : mini) * arr.size();
 }
 
 int main() { TimeMeasure _;
@@ -37,4 +31,13 @@ int main() { TimeMeasure _;
     cout << CountMinSteps({2, 1}) << '\n'; // 3
     cout << CountMinSteps({16, 16, 16}) << '\n'; // 7
     cout << CountMinSteps({2, 5}) << '\n'; // 6
+    cout << CountMinSteps({5, 3}) << '\n'; // 7
+    cout << CountMinSteps({5, 1}) << '\n'; // 6
+    cout << endl;
+    cout << CountMinSteps2({2, 3}) << '\n'; // 4
+    cout << CountMinSteps2({2, 1}) << '\n'; // 3
+    cout << CountMinSteps2({16, 16, 16}) << '\n'; // 7
+    cout << CountMinSteps2({2, 5}) << '\n'; // 6
+    cout << CountMinSteps2({5, 3}) << '\n'; // 7
+    cout << CountMinSteps2({5, 1}) << '\n'; // 6
 }
