9d97730710169ca0c3d16dea892f2014abdca90e
Artem Pushkin <idfumg@gmail.com>
2020-11-21 13:14:09 +0300

    Add new solutions to the Find k most frequent numbers

--- arrays/gfg/027_find_k_numbers_occurrences_given_array.cpp
+++ arrays/gfg/027_find_k_numbers_occurrences_given_array.cpp
@@ -1,31 +1,78 @@
 #include "../../template.hpp"
 
 void KNumsWithMostOccurences(vi arr, int k) {
-    int n = arr.size();
     unordered_map<int, int> tab;
-    for (int value : arr) {
+    for (const auto value : arr) {
         ++tab[value];
     }
+    priority_queue<pair<int, int>> pq;
+    for (const auto& [key, value] : tab) {
+        pq.push({value, key});
+    }
+    for (int i = 0; i < k and not pq.empty(); ++i) {
+        cout << pq.top().second << ' ';
+        pq.pop();
+    }
+    cout << endl;
+}
 
-    sort(arr.begin(), arr.end(),
-         [&](int a, int b) {
-             if (tab[a] == tab[b]) {
-                 return a < b;
-             }
-             return tab[a] < tab[b];
-         });
-
-    unordered_set<int> ans;
-    for (int i = n - 2; i >= 0; --i) {
-        ans.insert(arr[i]);
-        if (ans.size() == k) {
-            break;
+void KNumsWithMostOccurences2(vi arr, int k) {
+    unordered_map<int, int> tab;
+    for (const auto& value : arr) {
+        ++tab[value];
+    }
+    vi ans;
+    for (const auto& [elem, count] : tab) {
+        if (ans.size() < static_cast<size_t>(k)) {
+            ans.push_back(elem);
+            continue;
+        }
+        int mini = 0;
+        for (int j = 0; j < k; ++j) {
+            if (tab[ans[mini]] > tab[ans[j]] or (tab[ans[mini]] == tab[ans[j]] and ans[mini] > ans[j])) {
+                mini = j;
+            }
+        }
+        if (count > tab[ans[mini]] or (count == tab[ans[mini]] and elem > ans[mini])) {
+            ans[mini] = elem;
         }
     }
-    cout << ans << '\n';
+    for (int i = 0; i < k; ++i) {
+        cout << ans[i] << ' ';
+    }
+    cout << endl;
+}
+
+void KNumsWithMostOccurences3(vi arr, size_t k) {
+    unordered_map<int, int> tab;
+    for (const auto& value : arr) {
+        ++tab[value];
+    }
+    const auto comp = [&](int a, int b){ return tab[a] > tab[b]; };
+    priority_queue<int, vector<int>, decltype(comp)> pq(comp);
+    for (const auto& [elem, count] : tab) {
+        if (pq.size() < k) {
+            pq.push(elem);
+        }
+        else if (tab[pq.top()] < count or (tab[pq.top()] == count and pq.top() < elem)) {
+            pq.pop();
+            pq.push(elem);
+        }
+    }
+    while (not pq.empty()) {
+        cout << pq.top() << ' ';
+        pq.pop();
+    }
+    cout << endl;
 }
 
 int main() { TimeMeasure _;
-    KNumsWithMostOccurences({3, 1, 4, 4, 5, 2, 6, 1}, 2);
+    KNumsWithMostOccurences({3, 1, 4, 4, 5, 2, 6, 1}, 2); // O(nlogn), Om(n)
     KNumsWithMostOccurences({7, 10, 11, 5, 2, 5, 5, 7, 11, 8, 9}, 4);
+    cout << endl;
+    KNumsWithMostOccurences2({3, 1, 4, 4, 5, 2, 6, 1}, 2); // O(nk), Om(k)
+    KNumsWithMostOccurences2({7, 10, 11, 5, 2, 5, 5, 7, 11, 8, 9}, 4);
+    cout << endl;
+    KNumsWithMostOccurences3({3, 1, 4, 4, 5, 2, 6, 1}, 2); // O(n + klogk), Om(k)
+    KNumsWithMostOccurences3({7, 10, 11, 5, 2, 5, 5, 7, 11, 8, 9}, 4);
 }
