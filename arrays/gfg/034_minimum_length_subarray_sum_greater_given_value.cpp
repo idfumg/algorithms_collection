e95e7935137a064233040540ad43a5175b733d16
Artem Pushkin <idfumg@gmail.com>
2020-11-21 18:21:07 +0300

    Update the Min length subarray with sum greater then given value

--- arrays/gfg/034_minimum_length_subarray_sum_greater_given_value.cpp
+++ arrays/gfg/034_minimum_length_subarray_sum_greater_given_value.cpp
@@ -1,69 +1,31 @@
 #include "../../template.hpp"
 
-int tab(vi arr, int k) {
+int tab2(vi arr, int k) {
     int n = arr.size();
-    vi presum(n + 1);
-    for (int i = 1; i <= n; ++i) {
-        presum[i] = presum[i - 1] + arr[i - 1];
-    }
-
-    int mini = INF;
-    int minvalue = INF;
-    for (int i = 1; i <= n; ++i) {
-        for (int j = i; j <= n; ++j) {
-            int value = presum[j] - presum[i - 1];
-            if (value > k) {
-                if (minvalue > value) {
-                    minvalue = value;
-                    mini = j - i + 1;
-                }
-            }
-        }
-    }
-    return mini;
-}
-
-int tab2(vi arr, int k) { // Sliding Window
-    int n = arr.size();
-    int ans = 0;
-    int count = INF;
+    int window = 0;
+    int mincount = INF;
     int left = 0;
-    for (int i = 0; i < n; ++i) {
-        if (ans <= k) {
-            ans += arr[i];
-            if (ans > k) {
-                count = min(count, i - left);
-            }
+    int right = 0;
+    while (left < n or right < n) {
+        if (window <= k and right < n) {
+            window += arr[right++];
         }
         else {
-            while (ans > k and left != i) {
-                count = min(count, i - left);
-                ans -= arr[left++];
-            }
-            ans += arr[i];
+            window -= arr[left++];
+        }
+        if (window > k) {
+            mincount = min(mincount, right - left);
         }
     }
-    if (ans > k) {
-        return count;
-    }
-    return -1;
+    return mincount == INF ? -1 : mincount;
 }
 
 int main() { TimeMeasure _;
-    // O(n^2)
-    cout << tab({1, 4, 45, 6, 0, 19}, 51) << '\n'; // 3
-    cout << tab({1, 10, 5, 2, 7}, 9) << '\n'; // 1
-    cout << tab({1, 1, 5, 2, 7, 10}, 9) << '\n'; // 1
-    cout << tab({1, 1, 10}, 9) << '\n'; // 1
-    cout << tab({1, 11, 100, 1, 0, 200, 3, 2, 1, 250}, 280) << '\n'; // 4
-    cout << tab({1, 2, 4}, 8) << '\n'; // -1
-    cout << '\n';
-
-    // O(n)
     cout << tab2({1, 4, 45, 6, 0, 19}, 51) << '\n'; // 3
     cout << tab2({1, 10, 5, 2, 7}, 9) << '\n'; // 1
-    cout << tab({1, 1, 5, 2, 7, 10}, 9) << '\n'; // 1
-    cout << tab({1, 1, 10}, 9) << '\n'; // 1
+    cout << tab2({1, 1, 5, 2, 7, 10}, 9) << '\n'; // 1
+    cout << tab2({1, 1, 10}, 9) << '\n'; // 1
+    cout << tab2({10, 1, 2}, 9) << '\n'; // 1
     cout << tab2({1, 11, 100, 1, 0, 200, 3, 2, 1, 250}, 280) << '\n'; // 4
     cout << tab2({1, 2, 4}, 8) << '\n'; // -1
 }
