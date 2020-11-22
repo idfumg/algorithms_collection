7bf64e589b1f2a46ed536ac0c2946663ff7a58e3
Artem Pushkin <idfumg@gmail.com>
2020-11-22 12:24:11 +0300

    Update a Max sum array problem excluding some element

--- arrays/gfg/047_maximum_subarray_sum_excluding_certain_elements.cpp
+++ arrays/gfg/047_maximum_subarray_sum_excluding_certain_elements.cpp
@@ -1,25 +1,56 @@
 #include "../../template.hpp"
 
-void MaxSubArraySumExcludingSecondElements(vi a, vi b) {
+int BinarySearch(vi arr, int key) {
+    int n = arr.size();
+    int left = 0;
+    int right = n - 1;
+    while (left != right) {
+        int mid = left + (right - left) / 2;
+        if (arr[mid] < key) left = mid + 1;
+        else right = mid;
+    }
+    return arr[left] == key ? left : -1;
+}
+
+void MaxSubArraySumExcludingSecondElements(vi arr1, vi arr2) {
+    int m = arr1.size();
+    int n = arr2.size();
+    sort(arr2.begin(), arr2.end());
+    for (int i = 0; i < m; ++i) {
+        if (BinarySearch(arr2, arr1[i]) != -1) {
+            arr1[i] = -INF;
+        }
+    }
+    for (int i = 1; i < m; ++i) {
+        if (arr1[i] + arr1[i - 1] > arr1[i]) {
+            arr1[i] += arr1[i - 1];
+        }
+    }
+    cout << max(arr1) << endl;
+}
+
+void MaxSubArraySumExcludingSecondElements2(vi arr1, vi arr2) {
+    int m = arr1.size();
+    int n = arr2.size();
     unordered_set<int> tab;
-    for (int value : b) {
+    for (const int value : arr2) {
         tab.insert(value);
     }
-    int m = a.size();
-    vi dp(m);
-    dp[0] = tab.count(a[0]) ? 0 : a[0];
     for (int i = 1; i < m; ++i) {
-        if (tab.count(a[i])) {
-            dp[i] = 0;
+        if (tab.count(arr1[i])) {
+            arr1[i] = -INF;
         }
-        else {
-            dp[i] = dp[i - 1] + a[i];
+        else if (arr1[i] + arr1[i - 1] > arr1[i]) {
+            arr1[i] += arr1[i - 1];
         }
     }
-    cout << max(dp) << '\n';
+    cout << max(arr1) << endl;
 }
 
 int main() { TimeMeasure _;
-    MaxSubArraySumExcludingSecondElements({1, 7, -10, 6, 2}, {5, 6, 7, 1}); // 2
+    MaxSubArraySumExcludingSecondElements({1, 7, -10, 6, 2}, {5, 6, 7, 1}); // 2 // O(nlogn)
     MaxSubArraySumExcludingSecondElements({3, 4, 5, -4, 6}, {1, 8, 5}); // 7
+    cout << endl;
+    MaxSubArraySumExcludingSecondElements2({1, 7, -10, 6, 2}, {5, 6, 7, 1}); // 2 // O(n)
+    MaxSubArraySumExcludingSecondElements2({3, 4, 5, -4, 6}, {1, 8, 5}); // 7
 }
