f8338c88045e741330a1122a2a97399c17e7ef53
Artem Pushkin <idfumg@gmail.com>
2020-11-22 09:55:23 +0300

    Update Sort elements by frequency problem

--- arrays/gfg/041_sort_elements_by_frequency.cpp
+++ arrays/gfg/041_sort_elements_by_frequency.cpp
@@ -20,6 +20,34 @@ void SortByFrequency(vi arr) {
     cout << arr << '\n';
 }
 
+void SortByFrequency2(vi arr) {
+    int n = arr.size();
+    unordered_map<int, int> tab;
+    for (int value : arr) {
+        ++tab[value];
+    }
+    vi idxs(n, 0);
+    iota(idxs.begin(), idxs.end(), 0);
+    sort(idxs.begin(), idxs.end(),
+         [&](int i, int j) {
+             int a = arr[i];
+             int b = arr[j];
+             if (tab[a] == tab[b]) {
+                 return i < j;
+             }
+             return tab[a] > tab[b];
+         });
+    for (int i = 0; i < n; ++i) {
+        int idx = idxs[i];
+        cout << arr[idx] << ' ';
+    }
+    cout << endl;
+}
+
 int main() { TimeMeasure _;
-    SortByFrequency({2, 5, 2, 6, -1, 9999999, 5, 8, 8, 8});
+    SortByFrequency({2, 5, 2, 8, 5, 6, 8, 8}); // 8, 8, 8, 2, 2, 5, 5, 6
+    SortByFrequency({2, 5, 2, 6, -1, 9999999, 5, 8, 8, 8}); // 8, 8, 8, 2, 2, 5, 5, 6, -1, 9999999
+    cout << endl;
+    SortByFrequency2({2, 5, 2, 8, 5, 6, 8, 8}); // 8, 8, 8, 2, 2, 5, 5, 6
+    SortByFrequency2({2, 5, 2, 6, -1, 9999999, 5, 8, 8, 8}); // 8, 8, 8, 2, 2, 5, 5, 6, -1, 9999999
 }
