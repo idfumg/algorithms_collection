9604cf916c17c01029b81b1c0f1ccaf17a3bd21d
Artem Pushkin <idfumg@gmail.com>
2020-11-23 21:57:12 +0300

    Add gcd of numbers when one of them very large problem

---
+++ math/gfg/007_gcd_of_two_numbers_when_one_of_them_can_be_very_large_2.cpp
@@ -0,0 +1,15 @@
+#include "../../template.hpp"
+
+int GCDWithALargeNumber(int a, string b) {
+    int ans = 0;
+    int n = b.size();
+    for (int i = 0; i < n; ++i) {
+        ans = (ans * 10 + b[i] - '0') % a;
+    }
+    return gcd(a, ans);
+}
+
+int main() { TimeMeasure _;
+    cout << GCDWithALargeNumber(978, "89798763754892653453379597352537489494736") << endl; // 6
+    cout << GCDWithALargeNumber(1221, "1234567891011121314151617181920212223242526272829") << endl; // 3
+}
