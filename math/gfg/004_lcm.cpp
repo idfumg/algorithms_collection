610dc9699d35a7c3e3fe72bb51ba19b068ac87a2
Artem Pushkin <idfumg@gmail.com>
2020-11-23 21:55:38 +0300

    Add lcm problem

---
+++ math/gfg/004_lcm.cpp
@@ -0,0 +1,17 @@
+#include "../../template.hpp"
+
+int lcm(int a, int b) {
+    return (a * b) / std::gcd(a, b);
+}
+
+int lcm2(int a, int b) {
+    return std::lcm(a, b);
+}
+
+int main() { TimeMeasure _;
+    cout << lcm(15, 20) << endl; // 60
+    cout << lcm(5, 7) << endl; // 35
+    cout << endl;
+    cout << lcm2(15, 20) << endl; // 60
+    cout << lcm2(5, 7) << endl; // 35
+}
