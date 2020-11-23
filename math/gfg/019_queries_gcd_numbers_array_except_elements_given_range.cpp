3ae48cacaa9eed8c53109378edca54d97fae024e
Artem Pushkin <idfumg@gmail.com>
2020-11-23 22:01:35 +0300

    Add Find the queries of gcd numbers in an array i nthe given range

---
+++ math/gfg/019_queries_gcd_numbers_array_except_elements_given_range.cpp
@@ -0,0 +1,22 @@
+#include "../../template.hpp"
+
+int GcdInRange(vi arr, int a, int b) {
+    int n = arr.size();
+    vi prefix(n + 1);
+    vi suffix(n + 1);
+    for (int i = 1; i <= n; ++i) {
+        prefix[i] = gcd(prefix[i - 1], arr[i - 1]);
+    }
+    for (int i = n; i >= 0; --i) {
+        suffix[i] = gcd(suffix[i + 1], arr[i - 1]);
+    }
+    if (a == 0) return suffix[2];
+    if (b == n - 1) return prefix[n - 1];
+    return gcd(prefix[a], suffix[b]);
+}
+
+int main() { TimeMeasure _; __x();
+    cout << GcdInRange({2, 6, 9}, 0, 0) << endl; // 3
+    cout << GcdInRange({2, 6, 9}, 1, 1) << endl; // 1
+    cout << GcdInRange({2, 6, 9}, 1, 2) << endl; // 2
+}
