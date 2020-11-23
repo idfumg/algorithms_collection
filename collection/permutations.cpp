0ca7604271907f3ce01925de49def9c15da5cae8
Artem Pushkin <idfumg@gmail.com>
2020-11-23 22:05:05 +0300

    Add permutations finding algorithm in the collection

---
+++ collection/permutations.cpp
@@ -0,0 +1,33 @@
+#include "../template.hpp"
+
+void GenPermutationsRec(vi& arr, vb& choosen, vi& perm) {
+    if (perm.size() == arr.size()) {
+        cout << perm << endl;
+        return;
+    }
+    for (size_t i = 0; i < arr.size(); ++i) {
+        if (choosen[i]) continue;
+        choosen[i] = true;
+        perm.push_back(arr[i - 1]);
+        GenPermutationsRec(arr, choosen, perm);
+        perm.pop_back();
+        choosen[i] = false;
+    }
+}
+
+void GenPermutationsRec(vi arr) {
+    vi perm;
+    vb choosen(arr.size());
+    GenPermutationsRec(arr, choosen, perm);
+}
+
+void GenPermutationsIter(vi perm) {
+    do {
+        cout << perm << endl;
+    } while (next_permutation(perm.begin(), perm.end()));
+}
+
+int main() { TimeMeasure _;
+    GenPermutationsRec({1, 2, 6, 90, 0});
+    GenPermutationsIter({1, 2, 6, 90, 0});
+}
