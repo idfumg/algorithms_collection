6f7239fe92f0d8576cde9e2b85b27e193645b2b2
Artem Pushkin <idfumg@gmail.com>
2021-02-02 21:56:40 +0300

    Update prim's algorithm

--- collection/prim.cpp
+++ collection/prim.cpp
@@ -1,51 +1,52 @@
 #include "../template.hpp"
-#include "../collection/graph.hpp"
-#include "../collection/tree_node.hpp"
 
-struct Node {
-    int at;
-    int weight;
-    bool operator<(const Node& other) const { return weight > other.weight; }
-};
+using Graph = vvvi;
 
-void AddEdge(vector<vector<Node>>& graph, const int from, const int to, const int weight) {
-    graph[from].push_back({to, weight});
-    graph[to].push_back({from, weight});
+void AddEdge(Graph& graph, int from, int to, int cost = 0) {
+    graph[from].push_back({to, cost});
+    graph[to].push_back({from, cost});
 }
 
-void PrimsMinSpanningTree(const vector<vector<Node>>& graph, const int n) {
-    vi parent(n, -1);
-    vi key(n, INF);
-    vb intree(n);
-    priority_queue<Node> pq;
+struct Item {
+    int at;
+    int cost;
+    bool operator<(const Item& rhs) const { return cost > rhs.cost; }
+};
 
-    key[0] = 0;
+void PrimsMinSpanningTree(vvvi graph) {
+    int n = graph.size();
+    priority_queue<Item> pq;
     pq.push({0, 0});
-
+    vb visited(n);
+    vi dist(n, INF);
+    dist[0] = 0;
+    vi prev(n, -1);
     while (not pq.empty()) {
-        Node node = pq.top(); pq.pop();
-        intree[node.at] = true;
-        for (const Node& adj : graph[node.at]) {
-            if (not intree[adj.at] and key[adj.at] > adj.weight) {
-                key[adj.at] = adj.weight;
-                parent[adj.at] = node.at;
-                pq.push(adj);
+        Item item = pq.top(); pq.pop();
+        int at = item.at;
+        visited[at] = true;
+        for (vi adj : graph[at]) {
+            if (not visited[adj[0]]) {
+                if (dist[adj[0]] > adj[1]) {
+                    dist[adj[0]] = adj[1];
+                    prev[adj[0]] = at;
+                    pq.push({adj[0], adj[1]});
+                }
             }
         }
     }
-
     int total = 0;
-    for (int i = 1; i < n; ++i) {
-        cout << parent[i] << '-' << i << ' ' << key[i] << '\n';
-        total += key[i];
+    for (int at = 1; at < n; ++at) {
+        cout << prev[at] << '-' << at << ' ' << dist[at] << endl;
+        total += dist[at];
     }
-    debugn(total);
+    cout << "total: " << total << endl;
 }
 
 int main() { TimeMeasure _;
     {
         int n = 9;
-        vector<vector<Node>> graph(9);
+        Graph graph(n);
         AddEdge(graph, 0, 1, 4);
         AddEdge(graph, 0, 7, 8);
         AddEdge(graph, 1, 2, 8);
@@ -58,11 +59,11 @@ int main() { TimeMeasure _;
         AddEdge(graph, 2, 5, 4);
         AddEdge(graph, 5, 4, 10);
         AddEdge(graph, 3, 4, 9);
-        PrimsMinSpanningTree(graph, n);
+        PrimsMinSpanningTree(graph); // 37
     }
     {
         int n = 5;
-        vector<vector<Node>> graph(5);
+        Graph graph(n);
         AddEdge(graph, 0, 1, 2);
         AddEdge(graph, 0, 3, 6);
         AddEdge(graph, 1, 0, 2);
@@ -77,6 +78,6 @@ int main() { TimeMeasure _;
         AddEdge(graph, 4, 1, 5);
         AddEdge(graph, 4, 2, 7);
         AddEdge(graph, 4, 3, 9);
-        PrimsMinSpanningTree(graph, n);
+        PrimsMinSpanningTree(graph); // 16
     }
 }
