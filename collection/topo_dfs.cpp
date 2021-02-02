148fcc2730044dd8ea1cfece96b3e9c09baa3aab
Artem Pushkin <idfumg@gmail.com>
2021-02-02 21:57:50 +0300

    Update dfs topological sort algorithm

--- collection/topo_dfs.cpp
+++ collection/topo_dfs.cpp
@@ -1,58 +1,63 @@
 #include "../template.hpp"
-#include "../collection/graph.hpp"
-#include "../collection/tree_node.hpp"
 
-int dfs(vvvi& graph, vi& visited, vi& ordering, int at, int pos) {
+using Graph = vvi;
+
+void AddEdge(Graph& graph, int from, int to, int cost = 0) {
+    graph[from].push_back(to);
+    graph[to].push_back(from);
+}
+
+void dfs(vvvi& graph, vb& visited, vi& ordering, int& idx, int at) {
     visited[at] = true;
-    for (vi& adj : graph[at]) {
+    for (vi adj : graph[at]) {
         if (not visited[adj[0]]) {
-            pos = dfs(graph, visited, ordering, adj[0], pos);
+            dfs(graph, visited, ordering, idx, adj[0]);
         }
     }
-    ordering[pos] = at;
-    return pos - 1;
+    ordering[idx--] = at;
 }
 
 vi DFSBasedTopologicalSort(vvvi& graph) {
-    int n = graph.size(), pos = n - 1;
-    vi visited(n), ordering(n);
-    for (int i = 0; i < n; ++i) {
-        if (not visited[i]) {
-            pos = dfs(graph, visited, ordering, i, pos);
+    int n = graph.size();
+    int idx = n - 1;
+    vi ordering(n);
+    vb visited(n);
+    for (int at = 0; at < n; ++at) {
+        if (not visited[at]) {
+            dfs(graph, visited, ordering, idx, at);
         }
     }
     return ordering;
 }
 
-vi DAGShortestPath(vvvi& graph, const vi& ordering, int from) {
+vi DAGShortestPath(vvvi graph, vi ordering, int from) {
     int n = graph.size();
     vi dist(n, INF);
     dist[from] = 0;
-    for (int i = 0; i < n; ++i) {
-        int at = ordering[i];
-        if (dist[at] == INF) continue;
-        for (vi& adj : graph[at]) {
-            dist[adj[0]] = min(dist[adj[0]], dist[at] + adj[1]);
+    for (int at = 0; at < n; ++at) {
+        if (dist[ordering[at]] == INF) continue;
+        for (vi adj : graph[ordering[at]]) {
+            dist[adj[0]] = min(dist[adj[0]], dist[ordering[at]] + adj[1]);
         }
     }
     return dist;
 }
 
 int main() { TimeMeasure _;
-    vvvi graph(6);
-    graph[0].push_back({1, 3});
-    graph[0].push_back({2, 2});
-    graph[0].push_back({5, 3});
-    graph[1].push_back({3, 1});
-    graph[1].push_back({2, 6});
-    graph[2].push_back({3, 1});
-    graph[2].push_back({4, 10});
-    graph[3].push_back({4, 5});
-    graph[5].push_back({4, 7});
+    vvvi graph2(6);
+    graph2[0].push_back({1, 3});
+    graph2[0].push_back({2, 2});
+    graph2[0].push_back({5, 3});
+    graph2[1].push_back({3, 1});
+    graph2[1].push_back({2, 6});
+    graph2[2].push_back({3, 1});
+    graph2[2].push_back({4, 10});
+    graph2[3].push_back({4, 5});
+    graph2[5].push_back({4, 7});
 
-    const auto ordering = DFSBasedTopologicalSort(graph);
-    cout << ordering << endl; // 0, 5, 1, 2, 3, 4
+    const auto ordering2 = DFSBasedTopologicalSort(graph2);
+    cout << ordering2 << endl; // 0 1 5 2 3 4
 
-    const auto distances = DAGShortestPath(graph, ordering, 0);
-    cout << distances << endl; // 0 3 2 3 8 3
+    const auto distances2 = DAGShortestPath(graph2, ordering2, 0);
+    cout << distances2 << endl; // 0 3 2 3 8 3
 }
