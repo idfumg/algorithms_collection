d7ec03cc0b6e4fbac86f94eaa0a2a277db77523d
Artem Pushkin <idfumg@gmail.com>
2021-03-13 22:23:11 +0300

    Update graph main template source file

--- graph/gfg/main.cpp
+++ graph/gfg/main.cpp
@@ -2,9 +2,10 @@
 
 using Graph = vvi;
 
-void AddEdge(Graph& graph, int from, int to, int cost = 0) {
+[[maybe_unused]] void AddEdge(Graph& graph, int from, int to, int cost = 0) {
+    assert(cost != INF);
     graph[from].push_back(to);
-    // graph[to].push_back(from);
+    graph[to].push_back(from);
     // graph.push_back({from, to});
     // graph[from].push_back({to, cost});
     // graph[to].push_back({from, cost});
