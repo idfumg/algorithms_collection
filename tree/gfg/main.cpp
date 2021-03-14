45ca751a72798e36df93696be2e32d6792448ce4
Artem Pushkin <idfumg@gmail.com>
2021-03-14 13:22:38 +0300

    Update main.cpp template in a tree module

--- tree/gfg/main.cpp
+++ tree/gfg/main.cpp
@@ -1,20 +1,5 @@
 #include "../../template.hpp"
 
-struct TreeNode {
-    int value;
-    TreeNode* left;
-    TreeNode* right;
-    TreeNode(int value) : value(value), left(nullptr), right(nullptr) {}
-};
-
-void Inorder(TreeNode* root) {
-    if (not root) return;
-    Inorder(root->left);
-    cout << root->value << ' ';
-    Inorder(root->right);
-}
-
-
 
 int main() { TimeMeasure _; __x();
 
