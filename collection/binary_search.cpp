b84e91235085fbc2e6a376dfa044da6f1d4f5a20
Artem Pushkin <idfumg@gmail.com>
2021-03-14 10:18:08 +0300

    Update binary search algorithms

--- collection/binary_search.cpp
+++ collection/binary_search.cpp
@@ -1,39 +1,25 @@
 #include "../template.hpp"
 
-int BinarySearch(vi arr, int key) {
-    int n = arr.size(), low = 0, high = n - 1;
-
+int LowerBound(vi arr, int target) {
+    int low = 0, high = arr.size() - 1;
     while (low != high) {
         int mid = low + (high - low) / 2;
-        if (arr[mid] < key) low = mid + 1;
+        if (arr[mid] < target) low = mid + 1;
         else high = mid;
     }
-
-    return arr[low] == key ? low : -1;
-}
-
-int LowerBound(vi arr, int key) {
-    int n = arr.size(), low = 0, high = n - 1;
-
-    while (low != high) {
-        int mid = low + (high - low) / 2;
-        if (arr[mid] < key) low = mid + 1;
-        else high = mid;
-    }
-
     return low;
 }
 
-int UpperBound(vi arr, int key) {
-    int n = arr.size(), low = 0, high = n - 1;
+int UpperBound(vi arr, int target) {
+    int idx = LowerBound(arr, target);
+    if (idx == arr.size() - 1) return -1;
+    return idx + 1;
+}
 
-    while (low != high) {
-        int mid = low + (high - low) / 2;
-        if (arr[mid] <= key) low = mid + 1;
-        else high = mid;
-    }
-
-    return high;
+int BinarySearch(vi arr, int target) {
+    int idx = LowerBound(arr, target);
+    if (arr[idx] == target) return idx;
+    return -1;
 }
 
 void CeilingInSortedArray(vi arr, int key) {
@@ -66,4 +52,11 @@ int main() { TimeMeasure _;
     CeilingInSortedArray({1, 2, 8, 10, 10, 12, 19}, 10); // 10, 10
     CeilingInSortedArray({1, 2, 8, 10, 10, 12, 19}, 13); // 12, 19
     CeilingInSortedArray({1, 2, 8, 10, 10, 12, 19}, 19); // 19, 19
+    cout << endl;
+    assert(LowerBound({1,2,3,4,5,6,7,8,9}, 4) == 3);
+    assert(LowerBound({1,2,3,4,5,6,7,8,9}, 10) == 8);
+    assert(UpperBound({1,2,3,4,5,6,7,8,9}, 4) == 4);
+    assert(UpperBound({1,2,3,4,5,6,7,8,9}, 10) == -1);
+    assert(BinarySearch({1,2,3,4,5,6,7,8,9}, 4) == 3);
+    assert(BinarySearch({1,2,3,4,5,6,7,8,9}, 10) == -1);
 }
