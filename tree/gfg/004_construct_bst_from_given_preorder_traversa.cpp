b6e7a68b300182a5e0b0310cc69b204a4fad4045
Artem Pushkin <idfumg@gmail.com>
2021-03-18 21:39:35 +0300

    Update construct bst from given preorder traversa problem

--- tree/gfg/004_construct_bst_from_given_preorder_traversa.cpp
+++ tree/gfg/004_construct_bst_from_given_preorder_traversa.cpp
@@ -1,43 +1,41 @@
 #include "../../template.hpp"
 
 struct Node {
-    int value;
-    Node* left;
-    Node* right;
-    Node(int value) : value(value), left(nullptr), right(nullptr) {}
+    int value = -100;
+    Node* left = nullptr;
+    Node* right = nullptr;
+    Node(int value) : value(value) {}
 };
 
-void Inorder(Node* root) {
+void inorder(Node* root) {
     if (not root) return;
-    Inorder(root->left);
+    inorder(root->left);
     cout << root->value << ' ';
-    Inorder(root->right);
+    inorder(root->right);
 }
 
-Node* Construct(vi preorder, int i, int j) {
+Node* construct(vi pre, int i, int j) {
     if (i > j) return nullptr;
-
-    Node* root = new Node(preorder[i]);
-
-    int k = i + 1;
-    while (k <= j and preorder[k] < preorder[i]) ++k;
-    if (k > j) {
-        root->left = Construct(preorder, i + 1, j);
-        return root;
-    }
-
-    root->left = Construct(preorder, i + 1, k - 1);
-    root->right = Construct(preorder, k, j);
+    int rootValue = pre[i];
+    int key = i + 1;
+    while (key <= j and pre[key] <= rootValue) ++key;
+    Node* root = new Node(rootValue);
+    root->left = construct(pre, i + 1, key - 1);
+    root->right = construct(pre, key, j);
     return root;
 }
 
-void ConstructFromPreorder(vi preorder) {
-    Node* root = Construct(preorder, 0, preorder.size() - 1);
-    Inorder(root);
-    cout << endl;
+Node* ConstructFromPreorder(vi pre) {
+    return construct(pre, 0, pre.size() - 1);
 }
 
 int main() { TimeMeasure _; __x();
-    ConstructFromPreorder({40, 30, 35, 80, 100}); // 30 35 40 80 100
-    ConstructFromPreorder({40, 30, 32, 35, 80, 90, 100, 120}); // 30 32 35 40 80 90 100 120
+    {
+        Node* root = ConstructFromPreorder({40, 30, 35, 80, 100});
+        inorder(root); cout << endl; // 30 35 40 80 100
+    }
+    {
+        Node* root = ConstructFromPreorder({40, 30, 32, 35, 80, 90, 100, 120});
+        inorder(root); cout << endl; // 30 32 35 40 80 90 100 120
+    }
 }
