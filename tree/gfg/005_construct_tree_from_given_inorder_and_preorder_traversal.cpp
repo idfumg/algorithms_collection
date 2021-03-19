318fac57d00c7a3bc0dffafdb476c17f2f2e6fc6
Artem Pushkin <idfumg@gmail.com>
2021-03-19 22:26:39 +0300

    Update tree/gfg/005_construct_tree_from_given_inorder_and_preorder_traversal.cpp problem

--- tree/gfg/005_construct_tree_from_given_inorder_and_preorder_traversal.cpp
+++ tree/gfg/005_construct_tree_from_given_inorder_and_preorder_traversal.cpp
@@ -1,36 +1,34 @@
 #include "../../template.hpp"
 
 struct Node {
-    int value;
-    Node* left;
-    Node* right;
-    Node(int value) : value(value), left(nullptr), right(nullptr) {}
+    int value = INF;
+    Node* left = nullptr;
+    Node* right = nullptr;
+    Node(int value) : value(value) {}
 };
 
-void Inorder(Node* root) {
+void inorder(Node* root) {
     if (not root) return;
-    Inorder(root->left);
+    inorder(root->left);
     cout << root->value << ' ';
-    Inorder(root->right);
+    inorder(root->right);
 }
 
-Node* Make(vi inorder, vi preorder, int& rootIdx, int i, int j) {
-    if (i > j or rootIdx >= inorder.size()) return nullptr;
-    int rootValue = preorder[rootIdx++];
-
-    int inorderIdx = i;
-    for (; inorderIdx <= j and inorder[inorderIdx] != rootValue; ++inorderIdx) {}
-
+Node* construct(vi in, vi pre, int& idx, int i, int j) {
+    if (i > j) return nullptr;
+    int rootValue = pre[idx++];
+    int pivot = i;
+    while (pivot <= j and in[pivot] != rootValue) ++pivot;
     Node* root = new Node(rootValue);
-    root->left = Make(inorder, preorder, rootIdx, i, inorderIdx - 1);
-    root->right = Make(inorder, preorder, rootIdx, inorderIdx + 1, j);
+    root->left = construct(in, pre, idx, i, pivot - 1);
+    root->right = construct(in, pre, idx, pivot + 1, j);
     return root;
 }
 
-void BuildFromInorderAndPreorder(vi inorder, vi preorder) {
-    int rootIdx = 0;
-    Node* root = Make(inorder, preorder, rootIdx, 0, inorder.size() - 1);
-    Inorder(root);
+void BuildFromInorderAndPreorder(vi in, vi pre) {
+    int idx = 0;
+    Node* root = construct(in, pre, idx, 0, in.size() - 1);
+    inorder(root); cout << endl;
 }
 
 int main() { TimeMeasure _; __x();
