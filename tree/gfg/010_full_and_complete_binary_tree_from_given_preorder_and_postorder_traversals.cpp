e4d54e0419200ab416de235c6d2f906e6ffe77e8
Artem Pushkin <idfumg@gmail.com>
2021-03-20 09:54:40 +0300

    Update tree/gfg/010_full_and_complete_binary_tree_from_given_preorder_and_postorder_traversals.cpp

--- tree/gfg/010_full_and_complete_binary_tree_from_given_preorder_and_postorder_traversals.cpp
+++ tree/gfg/010_full_and_complete_binary_tree_from_given_preorder_and_postorder_traversals.cpp
@@ -1,10 +1,10 @@
 #include "../../template.hpp"
 
 struct Node {
-    int value;
-    Node* left;
-    Node* right;
-    Node(int value) : value(value), left(nullptr), right(nullptr) {}
+    int value = INF;
+    Node* left = nullptr;
+    Node* right = nullptr;
+    Node(int value) : value(value) {}
 };
 
 void inorder(Node* root) {
@@ -14,33 +14,25 @@ void inorder(Node* root) {
     inorder(root->right);
 }
 
-Node* construct(vi pre, vi post, int& idx, int from, int to) {
-    if (from > to or idx >= pre.size()) return nullptr;
-
-    int currentTreeRoot = pre[idx++];
-    Node* root = new Node(currentTreeRoot);
-
-    if (idx < pre.size()) {
-        int leftTreeRoot = pre[idx];
-        int i = from;
-        for (; i <= to and post[i] != leftTreeRoot; ++i){}
-        if (i <= to) {
-            root->left = construct(pre, post, idx, from, i);
-            root->right = construct(pre, post, idx, i + 1, to - 1);
-        }
-    }
-
+Node* construct(vi pre, vi post, int& idx, int i, int j) {
+    if (i > j) return nullptr;
+    int rootValue = pre[idx++];
+    if (idx > j) return new Node(rootValue);
+    int leftRootValue = pre[idx];
+    int pivot = i;
+    while (pivot < j and post[pivot] != leftRootValue) ++pivot;
+    Node* root = new Node(rootValue);
+    root->left = construct(pre, post, idx, i, pivot + 1);
+    root->right = construct(pre, post, idx, pivot + 1, j - 1);
     return root;
 }
 
-void construct(vi pre, vi post) {
+Node* construct(vi pre, vi post) {
     int idx = 0;
-    Node* root = construct(pre, post, idx, 0, pre.size() - 1);
-    inorder(root);
+    return construct(pre, post, idx, 0, pre.size() - 1);
 }
 
 int main() { TimeMeasure _; __x();
-    vi pre = {1, 2, 4, 8, 9, 5, 3, 6, 7};
-    vi post = {8, 9, 4, 5, 2, 6, 7, 3, 1};
-    construct(pre, post); // 8 4 9 2 5 1 6 3 7
+    Node* root = construct({1, 2, 4, 8, 9, 5, 3, 6, 7}, {8, 9, 4, 5, 2, 6, 7, 3, 1});
+    inorder(root); // // 8 4 9 2 5 1 6 3 7
 }
