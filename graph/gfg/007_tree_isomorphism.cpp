1a296d0518e9129fac3f83d81b8ba7b58f3fecad
Artem Pushkin <idfumg@gmail.com>
2021-03-25 23:36:02 +0300

    Update graph/gfg/007_tree_isomorphism.cpp

--- graph/gfg/007_tree_isomorphism.cpp
+++ graph/gfg/007_tree_isomorphism.cpp
@@ -1,104 +1,127 @@
 #include "../../template.hpp"
-#include "../collection/graph.hpp"
-#include "../collection/tree_node.hpp"
 
-vi FindTreeCenters(const Graph& graph) {
-    vi leaves, degree(graph.size());
-    for (int i = 0; i < graph.size(); ++i) {
-        degree[i] = graph.edges(i).size();
-        if (degree[i] <= 1) {
-            leaves.push_back(i);
-            degree[i] = 0;
+using Graph = vvi;
+
+void AddEdge(Graph& graph, int from, int to, int cost = 0) {
+    graph[from].push_back(to);
+    graph[to].push_back(from);
+}
+
+struct Tree {
+    int value;
+    vector<Tree*> nodes;
+    Tree(int value) : value(value), nodes() {}
+};
+
+Tree* MakeRoot(Graph graph, Tree* root, Tree* parent) {
+    for (int adj : graph[root->value]) {
+        if (parent and parent->value == adj) continue;
+        Tree* child = MakeRoot(graph, new Tree(adj), root);
+        root->nodes.push_back(child);
+    }
+    return root;
+}
+
+Tree* RootTree(Graph graph, int node) {
+    Tree* root = new Tree(node);
+    return MakeRoot(graph, root, nullptr);
+}
+
+string EncodeTree(Tree* root) {
+    if (not root) return "";
+    vector<string> encodings;
+    for (Tree* node : root->nodes) {
+        encodings.push_back(EncodeTree(node));
+    }
+    sort(encodings.begin(), encodings.end());
+    string ans;
+    for (const string& encoding : encodings) {
+        ans += encoding;
+    }
+    return "(" + ans + ")";
+}
+
+vi FindTreeCenters(Graph graph) {
+    int n = graph.size();
+
+    vi degree(n);
+    for (int at = 0; at < n; ++at) {
+        degree[at] = graph[at].size();
+    }
+
+    deque<int> leaves;
+    for (int at = 0; at < n; ++at) {
+        if (degree[at] <= 1) {
+            leaves.push_back(at);
+            degree[at] = 0;
         }
     }
+
     int total = leaves.size();
-    while (total < graph.size()) {
-        vi newleaves;
-        for (auto leaf : leaves) {
-            for (const auto& edge : graph.edges(leaf)) {
-                if (--degree[edge.to] == 1) {
-                    newleaves.push_back(edge.to);
-                }
+    while (not leaves.empty() and total < n) {
+        int at = leaves.front(); leaves.pop_front();
+        for (int adj : graph[at]) {
+            if (--degree[adj] == 1) {
+                leaves.push_back(adj);
             }
         }
-        leaves = newleaves;
-        total += leaves.size();
+        total += 1;
     }
-    return leaves;
+
+    vi centers;
+    for (int at = 0; at < n; ++at) {
+        if (degree[at] > 0) {
+            centers.push_back(at);
+        }
+    }
+    return centers;
 }
 
-TreeNode* BuildTree(const Graph& graph, TreeNode* node) {
-    for (const auto& edge : graph.edges(node->id)) {
-        if (node->parent && node->parent->id == edge.to) continue;
-        TreeNode* child = new TreeNode(edge.to, node);
-        node->children.push_back(child);
-        BuildTree(graph, child);
+bool TreesAreIsomorphic(Graph graph1, Graph graph2) {
+    const auto centers1 = FindTreeCenters(graph1);
+    const auto centers2 = FindTreeCenters(graph2);
+    if (centers1.empty()) return false;
+    const auto root1 = RootTree(graph1, centers1[0]);
+    const auto encode1 = EncodeTree(root1);
+    for (const auto& center : centers2) {
+        const auto root2 = RootTree(graph2, center);
+        const auto encode2 = EncodeTree(root2);
+        if (encode1 == encode2) {
+            return true;
+        }
     }
-    return node;
-}
-
-TreeNode* RootTree(const Graph& graph, int rootId) {
-    return BuildTree(graph, new TreeNode(rootId, nullptr));
-}
-
-string EncodeTree(TreeNode* root) {
-    if (not root) return "";
-    vector<string> labels;
-    for (const auto& child : root->children) {
-        labels.push_back(EncodeTree(child));
-    }
-    sort(labels);
-    string res;
-    for (const auto& label : labels) {
-        res += label;
-    }
-    return "(" + res + ")";
-}
-
-bool TreesAreIsomorphic(const Graph& tree1, const Graph& tree2) {
-    const auto centers1 = FindTreeCenters(tree1);
-    const auto centers2 = FindTreeCenters(tree2);
-    const auto root1 = RootTree(tree1, centers1[0]);
-    const auto encoded1 = EncodeTree(root1);
-
-    for (const auto center : centers2) {
-        const auto root2 = RootTree(tree2, center);
-        const auto encoded2 = EncodeTree(root2);
-        if (encoded1 == encoded2) return true;
-    }
-
     return false;
 }
 
 int main() { TimeMeasure _;
     Graph tree1(5);
-    tree1.addUndirectedEdge(2, 0);
-    tree1.addUndirectedEdge(3, 4);
-    tree1.addUndirectedEdge(2, 1);
-    tree1.addUndirectedEdge(2, 3);
+    AddEdge(tree1, 2, 0);
+    AddEdge(tree1, 3, 4);
+    AddEdge(tree1, 2, 1);
+    AddEdge(tree1, 2, 3);
 
     Graph tree2(5);
-    tree2.addUndirectedEdge(1, 0);
-    tree2.addUndirectedEdge(2, 4);
-    tree2.addUndirectedEdge(1, 3);
-    tree2.addUndirectedEdge(1, 2);
+    AddEdge(tree2, 1, 0);
+    AddEdge(tree2, 2, 4);
+    AddEdge(tree2, 1, 3);
+    AddEdge(tree2, 1, 2);
 
     if (!TreesAreIsomorphic(tree1, tree2)) {
         cout << "Oops, these tree should be isomorphic!" << endl;
     }
 
     Graph tree3(10);
-    tree3.addUndirectedEdge(0, 2);
-    tree3.addUndirectedEdge(0, 1);
-    tree3.addUndirectedEdge(0, 3);
-    tree3.addUndirectedEdge(2, 6);
-    tree3.addUndirectedEdge(2, 7);
-    tree3.addUndirectedEdge(1, 4);
-    tree3.addUndirectedEdge(1, 5);
-    tree3.addUndirectedEdge(5, 9);
-    tree3.addUndirectedEdge(3, 8);
+    AddEdge(tree3, 0, 2);
+    AddEdge(tree3, 0, 1);
+    AddEdge(tree3, 0, 3);
+    AddEdge(tree3, 2, 6);
+    AddEdge(tree3, 2, 7);
+    AddEdge(tree3, 1, 4);
+    AddEdge(tree3, 1, 5);
+    AddEdge(tree3, 5, 9);
+    AddEdge(tree3, 3, 8);
 
-    TreeNode* root3 = RootTree(tree3, 0);
+    Tree* root3 = RootTree(tree3, 0);
 
     if (EncodeTree(root3) != "(((())())(()())(()))") {
         cout << ("Tree encoding is wrong: " + EncodeTree(root3)) << endl;
