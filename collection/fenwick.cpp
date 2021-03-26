f373cf83e182943cf1c8bb13a43d83b2547b7b77
Artem Pushkin <idfumg@gmail.com>
2021-03-26 21:02:31 +0300

    Update collection/fenwick.cpp and fix a bug on a range update

--- collection/fenwick.cpp
+++ collection/fenwick.cpp
@@ -2,76 +2,65 @@
 
 using namespace std;
 
-vector<int> fenwick;
-
-void fenwick_init(const vector<int>& arr) {
-    const int n = arr.size() + 1;
-    fenwick.resize(n);
+void fenwick_init(vector<int>& tree, const vector<int>& arr) noexcept {
+    const int n = arr.size();
+    tree.clear();
+    tree.resize(n + 1);
     for (int i = 1; i <= n; ++i) {
-        fenwick[i] += arr[i - 1];
-        int parent = i + (i & -i);
-        if (parent <= n) {
-            fenwick[parent] += fenwick[i];
+        tree[i] += arr[i - 1];
+        if (const int parent = i + (i & -i); parent <= n) {
+            tree[parent] += tree[i];
         }
     }
 }
 
-int fenwick_sum(int i) {
+int fenwick_sum(const vector<int>& tree, int i) noexcept {
     int res = 0;
     for (; i > 0; i -= i & -i) {
-        res += fenwick[i];
+        res += tree[i];
     }
     return res;
 }
 
-int fenwick_sum(const int l, const int r) {
-    return fenwick_sum(r) - fenwick_sum(l - 1);
+int fenwick_sum(const vector<int>& tree, const int i, const int j) noexcept {
+    return fenwick_sum(tree, j) - fenwick_sum(tree, i - 1);
 }
 
-void fenwick_update(int i, int delta) {
-    const int n = fenwick.size();
-    for (; i <= n; i += i & -i) {
-        fenwick[i] += delta;
+void fenwick_update(vector<int>& tree, int i, const int delta) noexcept {
+    const int n = tree.size();
+    for (; i < n; i += (i & -i)) {
+        tree[i] += delta;
     }
 }
 
-void fenwick_update(int i, int j, int delta) {
-    fenwick_update(i, delta);
-    fenwick_update(j + 1, -delta);
-}
-
-void fenwick_set(int i, int value) {
-    int delta = value - fenwick_sum(i, i);
-    fenwick_update(i, delta);
-}
-
-void prefix_sums(const vector<int>& arr, const vector<vector<int>>& ranges) {
-    const int n = arr.size();
-    vector<int> prefix(n + 1);
-    for (int i = 1; i <= n; ++i) {
-        prefix[i] = prefix[i - 1] + arr[i - 1];
+void fenwick_update(vector<int>& tree, const int i, const int j, const int delta) noexcept {
+    for (int idx = i; idx <= j; ++idx) {
+        fenwick_update(tree, idx, delta);
     }
-    for (const auto& range : ranges) {
-        const int i = range[0];
-        const int j = range[1];
-        cout << prefix[j] - prefix[i - 1] << ' ';
-    }
-    cout << endl;
 }
 
-void fenwick_sums(const vector<int>& arr, const vector<vector<int>>& ranges) {
-    fenwick_init(arr);
-    for (const auto& range : ranges) {
-        const int i = range[0];
-        const int j = range[1];
-        cout << fenwick_sum(i, j) << ' ';
+void fenwick_set(vector<int>& tree, const int i, const int value) noexcept {
+    const int delta = value - fenwick_sum(tree, i, i);
+    fenwick_update(tree, i, delta);
+}
+
+void fenwick_sums(const vector<int>& tree, const vector<pair<int, int>>& ranges) noexcept {
+    for (const auto& [i, j] : ranges) {
+        cout << fenwick_sum(tree, i, j) << ' ';
     }
     cout << endl;
 }
 
 int main() {
-    vector<int> arr = {1, 2, 3, 4, 5, 6, 8, 9, 10, 11, 12};
-    prefix_sums(arr, {{1, 2}, {1, 3}, {2, 4}, {4, 8}});
-    fenwick_sums(arr, {{1, 2}, {1, 3}, {2, 4}, {4, 8}});
+    static const vector<int> arr = {1, 2, 3, 4, 5, 6, 8, 9, 10, 11, 12};
+    vector<int> tree;
+    fenwick_init(tree, arr);
+    fenwick_sums(tree, {{1, 2}, {1, 3}, {2, 4}, {4, 8}, {6, 6}}); // 3 6 9 32 6
+    fenwick_update(tree, 1, 3);
+    fenwick_sums(tree, {{1, 2}, {1, 3}, {2, 4}, {4, 8}, {6, 6}}); // 6 9 9 32 6
+    fenwick_update(tree, 1, 4, -1);
+    fenwick_sums(tree, {{1, 2}, {1, 3}, {2, 4}, {4, 8}, {6, 6}}); // 4 6 6 31 6
+    fenwick_set(tree, 6, 100);
+    fenwick_sums(tree, {{1, 2}, {1, 3}, {2, 4}, {4, 8}, {6, 6}}); // 4 6 6 125 100
     return 0;
 }
