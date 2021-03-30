7bebc4d2a462ecbd103e2ff4b53166425447fb66
Artem Pushkin <idfumg@gmail.com>
2021-03-30 21:09:38 +0300

    Update arrays/gfg/002_search_an_element_in_a_sorted_and_pivoted_array.cpp

--- arrays/gfg/002_search_an_element_in_a_sorted_and_pivoted_array.cpp
+++ arrays/gfg/002_search_an_element_in_a_sorted_and_pivoted_array.cpp
@@ -1,30 +1,14 @@
 #include "../../template.hpp"
 
-int SearchInRotatedAndSorted(vi arr, int key) {
-    int n = arr.size();
-    int left = 0;
-    int right = n - 1;
-    while (left <= right) {
-        int mid = left + (right - left) / 2;
-        if (arr[mid] == key) {
-            return mid;
-        }
-        else if (arr[mid] > key) {
-            if (arr[left] > key) {
-                left = mid + 1;
-            }
-            else {
-                right = mid - 1;
-            }
-        }
-        else {
-            if (arr[left] >= key and arr[right] < key) {
-                right = mid - 1;
-            }
-            else {
-                left = mid + 1;
-            }
-        }
+int SearchInRotatedAndSorted(vi arr, int target) {
+    int low = 0, high = arr.size() - 1;
+    while (low <= high) {
+        int mid = low + (high - low) / 2;
+        if (arr[mid] == target) return mid;
+        else if (arr[mid] > arr[0] and target < arr[0]) low = mid + 1; // left side
+        else if (arr[mid] < arr[0] and target >= arr[0]) high = mid - 1; // right side
+        else if (arr[mid] < target) low = mid + 1;
+        else if (arr[mid] > target) high = mid - 1;
     }
     return -1;
 }
@@ -38,6 +22,8 @@ int main() { TimeMeasure _;
     vi arr6 = {1, 2};
     vi arr7 = {1};
     vi arr8 = {};
+    vi arr9 = {12, 13, 14, 15, 16, 17, 18, 19, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
+    vi arr10 = {1, 1, 1, 1, 1, 2, 3, 3, 3, 3, 4, 4, 5, 5, 0, 0, 0};
 
     cout << SearchInRotatedAndSorted(arr1, 3) << '\n'; // 8
     cout << SearchInRotatedAndSorted(arr1, 10) << '\n'; // 5
@@ -49,4 +35,11 @@ int main() { TimeMeasure _;
     cout << SearchInRotatedAndSorted(arr6, 2) << '\n'; // 1
     cout << SearchInRotatedAndSorted(arr7, 1) << '\n'; // 0
     cout << SearchInRotatedAndSorted(arr8, 1) << '\n'; // -1
+    cout << SearchInRotatedAndSorted(arr9, 15) << '\n'; // 3
+    cout << SearchInRotatedAndSorted(arr9, 5) << '\n'; // 13
+    cout << SearchInRotatedAndSorted(arr9, 0) << '\n'; // 8
+    cout << SearchInRotatedAndSorted(arr9, 19) << '\n'; // 7
+    cout << SearchInRotatedAndSorted(arr9, 20) << '\n'; // -1
+    cout << SearchInRotatedAndSorted(arr9, -1) << '\n'; // -1
+    cout << SearchInRotatedAndSorted(arr10, 2) << '\n'; // 5
 }
