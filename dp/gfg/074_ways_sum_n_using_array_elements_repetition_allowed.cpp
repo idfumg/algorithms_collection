c4ea5023e581bec08d954440866c0b303e15b6bd
Artem Pushkin <idfumg@gmail.com>
2021-04-05 23:15:09 +0300

    Update dp/gfg/074_ways_sum_n_using_array_elements_repetition_allowed.cpp

--- dp/gfg/074_ways_sum_n_using_array_elements_repetition_allowed.cpp
+++ dp/gfg/074_ways_sum_n_using_array_elements_repetition_allowed.cpp
@@ -56,6 +56,35 @@ int tab2(vi arr, int sum) {
     return dp[1][0];
 }
 
+int rec(vi arr, int target, int n) {
+    if (target == 0) return 1;
+    if (n == 0 or target < 0) return 0;
+    return rec(arr, target, n - 1) + rec(arr, target - arr[n - 1], arr.size());
+}
+
+int rec3(vi arr, int target) {
+    return rec(arr, target, arr.size());
+}
+
+int tab3(vi arr, int target) {
+    int n = arr.size();
+    vvi dp(target + 1, vi(n + 1));
+    for (int i = 0; i <= target; ++i) {
+        for (int j = 1; j <= n; ++j) {
+            if (i == 0) {
+                dp[i][j] = 1;
+            }
+            else {
+                dp[i][j] = dp[i][j - 1];
+                if (i >= arr[j - 1]) {
+                    dp[i][j] += dp[i - arr[j - 1]][n];
+                }
+            }
+        }
+    }
+    return dp[target][n];
+}
+
 int main() { TimeMeasure _; __x();
     vi arr1 = {1, 5, 6}; int sum1 = 7; // 6
     vi arr2 = {12, 3, 1, 9}; int sum2 = 14; // 150
@@ -70,4 +99,10 @@ int main() { TimeMeasure _; __x();
     cout << '\n';
     cout << tab2(arr1, sum1) << endl;
     cout << tab2(arr2, sum2) << endl;
+    cout << '\n';
+    cout << rec3(arr1, sum1) << endl;
+    cout << rec3(arr2, sum2) << endl;
+    cout << '\n';
+    cout << tab3(arr1, sum1) << endl;
+    cout << tab3(arr2, sum2) << endl;
 }
