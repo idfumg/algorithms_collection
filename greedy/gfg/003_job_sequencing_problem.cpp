4f361f0c2f1f061d42a185d3435ff2c3ef6b8983
Artem Pushkin <idfumg@gmail.com>
2021-05-05 10:56:39 +0300

    Update greedy/gfg/003_job_sequencing_problem.cpp

--- greedy/gfg/003_job_sequencing_problem.cpp
+++ greedy/gfg/003_job_sequencing_problem.cpp
@@ -4,61 +4,29 @@ struct Job {
     char id;
     int deadline;
     int profit;
-    bool operator<(const Job& rhs) const { return profit > rhs.profit; }
 };
 
 using Jobs = vector<Job>;
 
-void JobScheduling(Jobs& jobs) {
-    int n = jobs.size();
-    vb slot(n);
-    vi result(n, -1);
-    sort(jobs);
-
-    for (int i = 0; i < n; ++i) {
-        for (int j = min(jobs[i].deadline, n) - 1; j >= 0; --j) {
-            if (not slot[j]) {
-                slot[j] = true;
-                result[j] = i;
+void JobScheduling(Jobs jobs) {
+    int n = 0;
+    for (const auto& job : jobs) {
+        n = max(n, job.deadline);
+    }
+    vi deadline(n + 1, 0);
+    sort(jobs.begin(), jobs.end(), [](auto a, auto b){return a.profit > b.profit;});
+    for (const auto& job : jobs) {
+        for (int idx = job.deadline; idx > 0; --idx) {
+            if (deadline[idx] == 0) {
+                deadline[idx] = job.profit;
                 break;
             }
         }
     }
-
-    int total = 0;
-    for (int i = 0; i < n; ++i) {
-        if (slot[i]) {
-            cout << jobs[result[i]].id << ' ';
-            total += jobs[result[i]].profit;
-        }
-    }
-    cout << ':' << ' ' << total << '\n';
+    cout << reduce(deadline.begin(), deadline.end(), 0) << endl;
 }
 
-int GetMaxDeadline(Jobs& jobs) {
-    int n = jobs.size(), ans = -INF;
-    for (int i = 0; i < n; ++i) {
-        ans = max(ans, jobs[i].deadline);
-    }
-    return ans;
-}
-
-void JobScheduling2(Jobs& jobs) {
-    sort(jobs);
-    int maxdeadline = GetMaxDeadline(jobs);
-    vi slot(maxdeadline + 1, 0);
-    for (Job& job : jobs) {
-        for (int i = job.deadline; i > 0; --i) {
-            if (not slot[i]) {
-                slot[i] = job.profit;
-                break;
-            }
-        }
-    }
-    cout << accumulate(slot.begin(), slot.end(), 0) << '\n';
-}
-
-int main() { TimeMeasure _;
+int main() { TimeMeasure _; __x();
     {
         Jobs jobs = {
             {'a', 2, 100},
@@ -67,8 +35,7 @@ int main() { TimeMeasure _;
             {'d', 1, 25},
             {'e', 3, 15},
         };
-        JobScheduling(jobs); // 142
-        JobScheduling2(jobs); // 142
+        JobScheduling(jobs); // 134
     }
     {
         Jobs jobs = {
@@ -80,6 +47,5 @@ int main() { TimeMeasure _;
             {'f', 2, 100},
         };
         JobScheduling(jobs); // 990
-        JobScheduling2(jobs); // 990
     }
 }
