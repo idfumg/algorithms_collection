85cad4e63bf189ba1c4f1e2ffb7fc68e9820dac6
Artem Pushkin <idfumg@gmail.com>
2021-05-05 10:56:39 +0300

    Update dp/gfg/132_maximum_sum_subarray_removing_one_element.cpp

--- dp/gfg/132_maximum_sum_subarray_removing_one_element.cpp
+++ dp/gfg/132_maximum_sum_subarray_removing_one_element.cpp
@@ -1,5 +1,37 @@
 #include "../../template.hpp"
 
+int rec(vi arr, int k, int n, int total) {
+    if (k < 0) return -INF;
+    if (n == 0) return total;
+    return max({
+            rec(arr, k, n - 1, 0),
+            rec(arr, k, n - 1, total + arr[n - 1]),
+            rec(arr, k - 1, n - 1, total),
+            total});
+}
+
+int rec(vi arr, int k) {
+    return rec(arr, k, arr.size(), 0);
+}
+
+int rec2(vi arr, int k, int n) {
+    if (k < 0) return -INF;
+    if (n == 0) return 0;
+    return max({
+            rec2(arr, k, n - 1) + arr[n - 1],
+            arr[n - 1],
+            rec2(arr, k - 1, n - 1)});
+}
+
+int rec2(vi arr, int k) {
+    int ans = -INF;
+    int n = arr.size();
+    for (int i = 1; i <= n; ++i) {
+        ans = max(ans, rec2(arr, k, i));
+    }
+    return ans;
+}
+
 int tab_logn(vi arr, int K) {
     int n = arr.size();
     int maxi = -INF, maxipos = 0;
@@ -156,23 +188,78 @@ deque<int> tab_elems(vi& arr, int K) {
     return elems;
 }
 
+int tab2(vi arr, int K) {
+    int n = arr.size();
+    int ans = 0;
+    vvvi dp(K + 1, vvi(n + 2, vi(n + 2)));
+    for (int i = n; i >= 1; --i) {
+        for (int j = 1; j <= n; ++j) {
+            if (i == j) dp[0][i][j] = arr[i - 1];
+            else if (j - i == 1) dp[0][i][j] = arr[i - 1] + arr[j - 1];
+            else dp[0][i][j] = dp[0][i][j - 1] + arr[j - 1];
+            ans = max(ans, dp[0][i][j]);
+        }
+    }
+    vector<vector<vector<si>>> used(K + 1, vector<vector<si>>(n + 2, vector<si>(n + 2)));
+    for (int k = 1; k <= K; ++k) {
+        for (int i = n; i >= 1; --i) {
+            for (int j = 1; j <= n; ++j) {
+                for (int p = i; p <= j; ++p) {
+                    if (dp[k - 1][i][j] - arr[p - 1] > dp[k - 1][i][j]) {
+                        if (not used[k - 1][i][j].count(p)) {
+                            dp[k][i][j] = dp[k - 1][i][j] - arr[p - 1];
+                            used[k][i][j] = used[k - 1][i][j];
+                            used[k][i][j].insert(p);
+                        }
+                    }
+                }
+                if (used[k][i][j].empty()) {
+                    used[k][i][j] = used[k - 1][i][j];
+                }
+                ans = max(ans, dp[k][i][j]);
+            }
+        }
+    }
+    return ans;
+}
+
 int main() { TimeMeasure _; __x();
-    vi arr1 = {1, 2, 3, -4, 5}; // 11
-    vi arr2 = {-2, -3, 4, -1, -2, 1, 5, -3}; // 9
-    vi arr3 = {-2, -3, 4, -1, -2, 1, 5, -3}; // 10
+    vi arr1 = {1, 2, 3, -4, 5};
+    vi arr2 = {-2, -3, 4, -1, -2, 1, 5, -3};
+    vi arr3 = {-2, -3, 4, -1, -2, 1, 5, -3};
+
+    cout << rec(arr1, 0) << '\n'; // 7
+    cout << rec(arr1, 1) << '\n'; // 11
+    cout << rec(arr2, 1) << '\n'; // 9
+    cout << rec(arr3, 2) << '\n'; // 10
+    cout << '\n';
+    cout << rec2(arr1, 0) << '\n'; // 7
+    cout << rec2(arr1, 1) << '\n'; // 11
+    cout << rec2(arr2, 1) << '\n'; // 9
+    cout << rec2(arr3, 2) << '\n'; // 10
+    cout << '\n';
+    cout << tab_logn(arr1, 0) << '\n';
     cout << tab_logn(arr1, 1) << '\n';
     cout << tab_logn(arr2, 1) << '\n';
     cout << tab_logn(arr3, 2) << '\n';
     cout << '\n';
+    cout << tab_logn_elems(arr1, 0) << '\n';
     cout << tab_logn_elems(arr1, 1) << '\n';
     cout << tab_logn_elems(arr2, 1) << '\n';
     cout << tab_logn_elems(arr3, 2) << '\n';
     cout << '\n';
+    cout << tab(arr1, 0) << '\n';
     cout << tab(arr1, 1) << '\n';
     cout << tab(arr2, 1) << '\n';
     cout << tab(arr3, 2) << '\n';
     cout << '\n';
+    cout << tab_elems(arr1, 0) << '\n';
     cout << tab_elems(arr1, 1) << '\n';
     cout << tab_elems(arr2, 1) << '\n';
     cout << tab_elems(arr3, 2) << '\n';
+    cout << '\n';
+    cout << tab2(arr1, 0) << '\n';
+    cout << tab2(arr1, 1) << '\n';
+    cout << tab2(arr2, 1) << '\n';
+    cout << tab2(arr3, 2) << '\n';
 }
