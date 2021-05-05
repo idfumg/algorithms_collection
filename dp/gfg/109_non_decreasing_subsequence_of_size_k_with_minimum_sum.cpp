d06f1cf4d0e96cdb80f2c4853048f8f5761abc9b
Artem Pushkin <idfumg@gmail.com>
2021-05-05 10:56:39 +0300

    Update dp/gfg/109_non_decreasing_subsequence_of_size_k_with_minimum_sum.cpp

--- dp/gfg/109_non_decreasing_subsequence_of_size_k_with_minimum_sum.cpp
+++ dp/gfg/109_non_decreasing_subsequence_of_size_k_with_minimum_sum.cpp
@@ -54,6 +54,54 @@ int tab(vi& arr, int K) {
     return min(dp[K]);
 }
 
+int rec(vi arr, int k, int n, int prev) {
+    if (k == 0) return 0;
+    if (n == 0) return INF;
+    if (arr[n - 1] <= arr[prev - 1]) {
+        return min(rec(arr, k - 1, n - 1, n) + arr[n - 1],
+                   rec(arr, k, n - 1, prev));
+    }
+    return rec(arr, k, n - 1, prev);
+}
+
+int rec(vi arr, int k) {
+    int ans = INF;
+    int n = arr.size();
+    for (int i = 1; i <= n; ++i) {
+        ans = min(ans, rec(arr, k - 1, i - 1, i) + arr[i - 1]);
+    }
+    return ans;
+}
+
+int tab2(vi arr, int K) {
+    int n = arr.size();
+    vvvi dp(K + 1, vvi(n + 1, vi(n + 1)));
+    for (int k = 0; k <= K; ++k) {
+        for (int i = 0; i <= n; ++i) {
+            for (int j = 0; j <= n; ++j) {
+                if (k == 0) {
+                    dp[k][i][j] = 0;
+                }
+                else if (i == 0) {
+                    dp[k][i][j] = INF;
+                }
+                else if (arr[i - 1] <= arr[j - 1]) {
+                    dp[k][i][j] = min((k > 0 ? dp[k - 1][i - 1][i] : 0) + arr[i - 1],
+                                      dp[k][i - 1][j]);
+                }
+                else {
+                    dp[k][i][j] = dp[k][i - 1][j];
+                }
+            }
+        }
+    }
+    int ans = INF;
+    for (int i = 1; i <= n; ++i) {
+        ans = min(ans, dp[K - 1][i - 1][i] + arr[i - 1]);
+    }
+    return ans;
+}
+
 int main() { TimeMeasure _; __x();
     vi arr1 = {58, 12, 11, 12, 82, 30, 20, 77, 16, 86}; int k1 = 3;
     vi arr2 = {58, 12, 11, 12, 82, 30, 20, 77, 16, 86}; int k2 = 4;
@@ -66,4 +114,12 @@ int main() { TimeMeasure _; __x();
     cout << tab(arr1, k1) << '\n'; // 39
     cout << tab(arr2, k2) << '\n'; // 120
     cout << tab(arr3, k3) << '\n'; // 206
+    cout << '\n';
+    cout << rec(arr1, k1) << '\n'; // 39
+    cout << rec(arr2, k2) << '\n'; // 120
+    cout << rec(arr3, k3) << '\n'; // 206
+    cout << '\n';
+    cout << tab2(arr1, k1) << '\n'; // 39
+    cout << tab2(arr2, k2) << '\n'; // 120
+    cout << tab2(arr3, k3) << '\n'; // 206
 }
