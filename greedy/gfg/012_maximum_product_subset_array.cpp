cda51eec16b36d053d1920124bebb1d887f0ba63
Artem Pushkin <idfumg@gmail.com>
2021-05-05 10:56:39 +0300

    Update greedy/gfg/012_maximum_product_subset_array.cpp

--- greedy/gfg/012_maximum_product_subset_array.cpp
+++ greedy/gfg/012_maximum_product_subset_array.cpp
@@ -2,7 +2,7 @@
 
 int MaximumProductSubset(vi arr) {
     int n = arr.size();
-    if (n == 0) return -INF;
+    if (n == 0) return 0;
     int total = -INF;
     int maxNegative = -INF;
     int negatives = 0;
@@ -21,6 +21,33 @@ int MaximumProductSubset(vi arr) {
     return total / maxNegative;
 }
 
+int MaximumProductSubset2(vi arr) {
+    sort(arr.begin(), arr.end(), greater<int>());
+    int n = arr.size();
+    if (n == 0) return 0;
+
+    int ans = 1;
+    int i = 0;
+    while (i < n and arr[i] > 0) {
+        ans *= arr[i++];
+    }
+
+    while (i < n and arr[i] == 0) {
+        ++i;
+    }
+
+    i = n - 1;
+    while (i >= 0 and arr[i] < 0) {
+        ans *= arr[i--];
+    }
+
+    if (ans < 0 and i < n - 1) {
+        ans /= arr[i + 1];
+    }
+
+    return arr[0] > 0 ? ans : arr[0];
+}
+
 int main() { TimeMeasure _;
     vi arr1 = { -1, -1, -2, 4, 3 }; // 24
     vi arr2 = { -1, 0 }; // 0
@@ -28,4 +55,8 @@ int main() { TimeMeasure _;
     cout << MaximumProductSubset(arr1) << '\n';
     cout << MaximumProductSubset(arr2) << '\n';
     cout << MaximumProductSubset(arr3) << '\n';
+    cout << endl;
+    cout << MaximumProductSubset2(arr1) << '\n';
+    cout << MaximumProductSubset2(arr2) << '\n';
+    cout << MaximumProductSubset2(arr3) << '\n';
 }
