de6b81d0d4171b61dc84a647894f6850fdcae627
Artem Pushkin <idfumg@gmail.com>
2021-05-05 10:56:39 +0300

    Update graph/gfg/047_stepping_numbers.cpp

--- graph/gfg/047_stepping_numbers.cpp
+++ graph/gfg/047_stepping_numbers.cpp
@@ -1,60 +1,70 @@
 #include "../../template.hpp"
 
-bool IsSteppingNumber(int n) {
-    int first = -1;
-    while (n > 0) {
-        int second = n % 10;
-        if (first != -1) {
-            if (abs(first - second) != 1) {
-                return false;
-            }
+using Graph = vvi;
+
+void AddEdge(Graph& graph, int from, int to, int cost = 0) {
+    graph[from].push_back(to);
+    // graph[to].push_back(from);
+    // graph.push_back({from, to});
+    // graph[from].push_back({to, cost});
+    // graph[to].push_back({from, cost});
+    // graph.push_back({from, to, cost});
+    // graph[from][to] = cost;
+    // graph[to][from] = cost;
+}
+
+bool isStepping(int n) {
+    int prev = n % 10;
+    n /= 10;
+    for (; n > 0; n /= 10) {
+        int curr = n % 10;
+        if (abs(curr - prev) != 1) {
+            return false;
         }
-        first = second;
-        n /= 10;
+        prev = curr;
     }
     return true;
 }
 
-vi Brute(int from, int to) { // O(n^2)
-    vi res;
-    for (int i = from; i <= to; ++i) { // O(n)
-        if (IsSteppingNumber(i)) { // O(n)
-            res.push_back(i);
+vi brute(int from, int to) {
+    vi ans;
+    for (int i = from; i <= to; ++i) {
+        if (isStepping(i)) {
+            ans.push_back(i);
         }
     }
-    return res;
+    return ans;
 }
 
-si bfs(int from, int to) { // O(n)
-    qi q;
-    si ans;
-
-    for (int i = 0; i <= 9; ++i) {
-        q.push(i);
+vi bfs(int a, int b) {
+    deque<int> q;
+    for (int i = 0; i < 10; ++i) {
+        q.push_back(i);
     }
+
+    vb visited(b + 1);
 
     while (not q.empty()) {
-        int at = q.front(); q.pop();
-
-        if (at < 0 or at > to) {
-            continue;
-        }
-
-        if (at >= from and at <= to) {
-            ans.insert(at);
-        }
-
-        for (int adj : {at * 10 + (at % 10 - 1), at * 10 + (at % 10 + 1)}) {
-            q.push(adj);
+        int at = q.front(); q.pop_front();
+        if (at >= 0 and at <= b) {
+            visited[at] = true;
+            q.push_back(at * 10 + at + 1);
+            q.push_back(at * 10 + at - 1);
         }
     }
 
+    vi ans;
+    for (int i = 0; i <= b; ++i) {
+        if (visited[i] and i >= a and i <= b) {
+            ans.push_back(i);
+        }
+    }
     return ans;
 }
 
 int main() { TimeMeasure _;
-    cout << Brute(0, 21) << endl;
-    cout << Brute(10, 15) << endl;
+    cout << brute(0, 21) << endl; // 0 1 2 3 4 5 6 7 8 9 10 12 21
+    cout << brute(10, 15) << endl; // 10, 12
     cout << bfs(0, 21) << endl;
     cout << bfs(10, 15) << endl;
 }
