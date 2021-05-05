8a3745aba04569db0090f5471c1170383556d631
Artem Pushkin <idfumg@gmail.com>
2021-05-05 10:56:38 +0300

    Update arrays/gfg/034_minimum_length_subarray_sum_greater_given_value.cpp

--- arrays/gfg/034_minimum_length_subarray_sum_greater_given_value.cpp
+++ arrays/gfg/034_minimum_length_subarray_sum_greater_given_value.cpp
@@ -1,31 +1,43 @@
 #include "../../template.hpp"
 
-int tab2(vi arr, int k) {
-    int n = arr.size();
+int SmallestSubarrayWithGreaterSum(vi arr, int target) {
     int window = 0;
-    int mincount = INF;
-    int left = 0;
-    int right = 0;
-    while (left < n or right < n) {
-        if (window <= k and right < n) {
-            window += arr[right++];
+    int windowSize = 0;
+    int ans = INF;
+
+    int n = arr.size();
+    int i = 0;
+    while (i < n) {
+        if (window <= target) {
+            window += arr[i++];
+            ++windowSize;
         }
         else {
-            window -= arr[left++];
+            window -= arr[i - windowSize];
+            --windowSize;
         }
-        if (window > k) {
-            mincount = min(mincount, right - left);
+        if (window > target) {
+            ans = min(ans, windowSize);
         }
     }
-    return mincount == INF ? -1 : mincount;
+
+    while (window > target) {
+        window -= arr[i - windowSize];
+        --windowSize;
+        if (window > target) {
+            ans = min(ans, windowSize);
+        }
+    }
+
+    return ans;
 }
 
 int main() { TimeMeasure _;
-    cout << tab2({1, 4, 45, 6, 0, 19}, 51) << '\n'; // 3
-    cout << tab2({1, 10, 5, 2, 7}, 9) << '\n'; // 1
-    cout << tab2({1, 1, 5, 2, 7, 10}, 9) << '\n'; // 1
-    cout << tab2({1, 1, 10}, 9) << '\n'; // 1
-    cout << tab2({10, 1, 2}, 9) << '\n'; // 1
-    cout << tab2({1, 11, 100, 1, 0, 200, 3, 2, 1, 250}, 280) << '\n'; // 4
-    cout << tab2({1, 2, 4}, 8) << '\n'; // -1
+    cout << SmallestSubarrayWithGreaterSum({1, 4, 45, 6, 0, 19}, 51) << '\n'; // 3
+    cout << SmallestSubarrayWithGreaterSum({1, 10, 5, 2, 7}, 9) << '\n'; // 1
+    cout << SmallestSubarrayWithGreaterSum({1, 1, 5, 2, 7, 10}, 9) << '\n'; // 1
+    cout << SmallestSubarrayWithGreaterSum({1, 1, 10}, 9) << '\n'; // 1
+    cout << SmallestSubarrayWithGreaterSum({10, 1, 2}, 9) << '\n'; // 1
+    cout << SmallestSubarrayWithGreaterSum({1, 11, 100, 1, 0, 200, 3, 2, 1, 250}, 280) << '\n'; // 4
+    cout << SmallestSubarrayWithGreaterSum({1, 2, 4}, 8) << '\n'; // -1
 }
