852ae5a1ca6f39d4109a2fde2bf23efb5adda400
Artem Pushkin <idfumg@gmail.com>
2021-05-05 10:56:39 +0300

    Update graph/gfg/065_count_possible_paths_source_destination_exactly_k_edges.cpp

--- graph/gfg/065_count_possible_paths_source_destination_exactly_k_edges.cpp
+++ graph/gfg/065_count_possible_paths_source_destination_exactly_k_edges.cpp
@@ -2,53 +2,64 @@
 
 using Graph = vvi;
 
+void AddEdge(Graph& graph, int from, int to, int cost = 0) {
+    graph[from].push_back(to);
+    // graph[to].push_back(from);
+    // graph.push_back({from, to});
+    // graph[from].push_back({to, cost});
+    // graph[to].push_back({from, cost});
+    // graph.push_back({from, to, cost});
+    // graph[from][to] = cost;
+    // graph[to][from] = cost;
+}
+
 void CountWalkWithKEdgesDP(Graph& graph, int from, int to, int K) {
     int n = graph.size();
-    vvvi dp(K + 1, vvi(n, vi(n)));
+    vvvi dp(n, vvi(n, vi(K + 1, INF)));
     for (int i = 0; i < n; ++i) {
         for (int j = 0; j < n; ++j) {
-            if (graph[i][j]) dp[1][i][j] = 1;
-            else if (i == j) dp[0][i][j] = 1;
+            if (graph[i][j]) {
+                dp[i][j][1] = graph[i][j];
+            }
         }
     }
     for (int k = 2; k <= K; ++k) {
         for (int i = 0; i < n; ++i) {
             for (int j = 0; j < n; ++j) {
                 for (int p = 0; p < n; ++p) {
-                    if (graph[p][j]) {
-                        dp[k][i][j] += dp[k - 1][i][p];
-                    }
+                    if (j == i or j == p) continue;
+                    dp[i][p][k] = min(dp[i][p][k], dp[i][j][k - 1] + dp[j][p][1]);
                 }
             }
         }
     }
-    cout << dp[K][from][to] << '\n';
+    cout << dp[from][to][K] << endl;
 }
 
-void dfs(Graph& graph, vb& visited, int at, int to, int k, int& count) {
+void dfs(Graph& graph, vb& visited, int at, int to, int K, int& ans) {
+    if (K == 0 and at == to) {
+        ++ans;
+        return;
+    }
+    if (K < 0) {
+        return;
+    }
+    visited[at] = true;
     int n = graph.size();
-    if (at == to and k == 0) {
-        ++count;
-        return;
-    }
-    if (k <= 0) {
-        return;
-    }
-    for (int i = 0; i < n; ++i) {
-        if (graph[at][i] and not visited[i]) {
-            visited[i] = true;
-            dfs(graph, visited, i, to, k - 1, count);
-            visited[i] = false;
+    for (int adj = 0; adj < n; ++adj) {
+        if (not visited[adj]) {
+            dfs(graph, visited, adj, to, K - 1, ans);
         }
     }
-
+    visited[at] = false;
 }
 
 void CountWalkWithKEdgesDFS(Graph& graph, int from, int to, int K) {
-    int n = graph.size(), count = 0;
+    int n = graph.size();
     vb visited(n);
-    dfs(graph, visited, from, to, K, count);
-    cout << count << '\n';
+    int ans = 0;
+    dfs(graph, visited, from, to, K, ans);
+    cout << ans << endl;
 }
 
 int main() { TimeMeasure _;
