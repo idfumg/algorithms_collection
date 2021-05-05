89759007456790064c745ea1991ab34732bf0772
Artem Pushkin <idfumg@gmail.com>
2021-05-05 10:56:38 +0300

    Update arrays/gfg/026_k_smallest_elements_order_using_o1_extra_space.cpp

--- arrays/gfg/026_k_smallest_elements_order_using_o1_extra_space.cpp
+++ arrays/gfg/026_k_smallest_elements_order_using_o1_extra_space.cpp
@@ -2,51 +2,19 @@
 
 void KSmallestElems(vi arr, int k) {
     int n = arr.size();
-    stable_sort(arr.begin(), arr.end());
-    for (int i = 0; i < k and i < n; ++i) {
-        cout << arr[i] << ' ';
-    }
-    cout << endl;
-}
-
-void KSmallestElems2(vi arr, int k) {
-    int n = arr.size();
-    for (int i = 0; i < k and i < n; ++i) {
-        int idx = i;
-        for (int j = i + 1; j < n; ++j) {
-            if (arr[j] < arr[idx]) {
-                idx = j;
-            }
-        }
-        if (idx != i) {
-            swap(arr[i], arr[idx]);
-        }
-    }
-    for (int i = 0; i < k; ++i) {
-        cout << arr[i] << ' ';
-    }
-    cout << endl;
-}
-
-void KSmallestElems3(vi arr, int k) {
-    int n = arr.size();
     for (int i = k; i < n; ++i) {
-        int maxvalue = arr[0];
-        int maxpos = 0;
-        for (int j = 1; j < k; ++j) {
-            if (arr[j] > maxvalue) {
-                maxvalue = arr[j];
-                maxpos = j;
+        int maxi = -INF, maxipos = INF;
+        for (int j = 0; j < k; ++j) {
+            if (arr[j] > maxi) {
+                maxi = arr[j];
+                maxipos = j;
             }
         }
-        if (maxvalue > arr[i]) {
-            arr[maxpos] = arr[i];
+        if (arr[i] < maxi) {
+            arr[maxipos] = arr[i];
         }
     }
-    for (int i = 0; i < k; ++i) {
-        cout << arr[i] << ' ';
-    }
-    cout << endl;
+    for_each(arr.begin(), arr.begin() + k, [](int v){cout << v << ' ';}); cout << endl;
 }
 
 int main() { TimeMeasure _;
@@ -55,10 +23,4 @@ int main() { TimeMeasure _;
 
     KSmallestElems(arr1, k1); // O(nlogn) + Om(n)
     KSmallestElems(arr2, k2);
-    cout << '\n';
-    KSmallestElems2(arr1, k1); // O(n*k) + Om(1)
-    KSmallestElems2(arr2, k2);
-    cout << '\n';
-    KSmallestElems3(arr1, k1); // O(n) + Om(1)
-    KSmallestElems3(arr2, k2);
 }
