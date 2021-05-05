a96fafcb30edd9452c4109c432271767f229ff66
Artem Pushkin <idfumg@gmail.com>
2021-05-05 10:56:38 +0300

    Update dp/gfg/089_count_palindromic_subsequence_given_string.cpp

--- dp/gfg/089_count_palindromic_subsequence_given_string.cpp
+++ dp/gfg/089_count_palindromic_subsequence_given_string.cpp
@@ -1,21 +1,13 @@
 #include "../../template.hpp"
 
 int rec(string s, int i, int j) {
-    if (i > j) {
-        return 0;
-    }
-    if (i == j) {
-        return 1;
-    }
-    if (s[i - 1] == s[j - 1]) {
-        return 1 + rec(s, i + 1, j) + rec(s, i, j - 1);
-    }
+    if (i > j) return 0;
+    if (s[i - 1] == s[j - 1]) return 1 + rec(s, i + 1, j) + rec(s, i, j - 1);
     return rec(s, i + 1, j) + rec(s, i, j - 1) - rec(s, i + 1, j - 1);
 }
 
 int rec(string s) {
-    int n = s.size();
-    return rec(s, 1, n);
+    return rec(s, 1, s.size());
 }
 
 int tab(string s) {
@@ -23,24 +15,15 @@ int tab(string s) {
     vvi dp(n + 2, vi(n + 1));
     for (int i = n; i >= 1; --i) {
         for (int j = 1; j <= n; ++j) {
-            if (i > j) {
-                dp[i][j] = 0;
-            }
-            else if (i == j) {
-                dp[i][j] = 1;
-            }
-            else if (s[i - 1] == s[j - 1]) {
-                dp[i][j] = 1 + dp[i + 1][j] + dp[i][j - 1];
-            }
-            else {
-                dp[i][j] = dp[i + 1][j] + dp[i][j - 1] - dp[i + 1][j - 1];
-            }
+            if (i > j) dp[i][j] = 0;
+            else if (s[i - 1] == s[j - 1]) dp[i][j] = 1 + dp[i + 1][j] + dp[i][j - 1];
+            else dp[i][j] = dp[i + 1][j] + dp[i][j - 1] - dp[i + 1][j - 1];
         }
     }
-    return dp[1][n];
+        return dp[1][n];
 }
 
-int main() { TimeMeasure _; __x();
+int main() { // TimeMeasure _; __x();
     static const string s1 = "abcd"; // 4
     static const string s2 = "aab"; // 4
     static const string s3 = "aaaa"; // 15
