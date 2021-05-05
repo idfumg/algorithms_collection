710ac6f320a7d5f373d84bbe2ecb5d8492190cb4
Artem Pushkin <idfumg@gmail.com>
2021-05-05 10:56:39 +0300

    Update graph/gfg/043_minimum_number_operation_required_convert_number_x_y.cpp

--- graph/gfg/043_minimum_number_operation_required_convert_number_x_y.cpp
+++ graph/gfg/043_minimum_number_operation_required_convert_number_x_y.cpp
@@ -1,51 +1,35 @@
 #include "../../template.hpp"
 
-struct Cell {
-    int num;
-    int dist;
-};
-
 int MinimumOpsToTransfrom(int from, int to) {
-    unordered_set<int> visited;
-    deque<Cell> q; q.push_back(Cell{from, 0});
-
-    while (not q.empty()) {
-        Cell at = q.front(); q.pop_front();
-        if (at.num == to) return at.dist;
-        visited.insert(at.num);
-        if (not visited.count(at.num * 2)) {
-            q.push_back(Cell{at.num * 2, at.dist + 1});
-        }
-        if (at.num - 1 >= 0 and not visited.count(at.num - 1)) {
-            q.push_back(Cell{at.num - 1, at.dist + 1});
-        }
-    }
-    return -1;
-}
-
-int MinimumOpsToTransfrom2(int x, int y) {
-    qi q;
-    q.push(x);
+    deque<int> q;
+    q.push_back(from);
 
     unordered_map<int, int> dist;
-    dist[x] = 0;
+    dist[from] = 0;
+
+    unordered_map<int, bool> visited;
 
     while (not q.empty()) {
-        int at = q.front(); q.pop();
-
-        for (int adj : {at - 1, at * 2}) {
-            if (adj >= 0 and adj <= 2 * y and not dist.count(adj)) {
-                dist[adj] = dist[at] + 1;
-                q.push(adj);
+        int at = q.front(); q.pop_front();
+        if (at == to) {
+            return dist[to];
+        }
+        visited[at] = true;
+        for (int adj : {2 * at, at - 1}) {
+            if (not visited[adj]) {
+                if (not dist.count(adj)) {
+                    dist[adj] = INF;
+                }
+                dist[adj] = min(dist[adj], dist[at] + 1);
+                q.push_back(adj);
             }
         }
     }
-    return dist[y];
+
+    return dist[to];
 }
 
 int main() { TimeMeasure _;
     cout << MinimumOpsToTransfrom(4, 7) << '\n'; // 2
     cout << MinimumOpsToTransfrom(2, 5) << '\n'; // 4
-    cout << MinimumOpsToTransfrom2(4, 7) << '\n'; // 2
-    cout << MinimumOpsToTransfrom2(2, 5) << '\n'; // 4
 }
