13f89619a8fce8ba7e25d8079985a62b8d70ee31
Artem Pushkin <idfumg@gmail.com>
2021-05-05 10:56:39 +0300

    Update greedy/gfg/008_minimum_swaps_bracket_balancing.cpp

--- greedy/gfg/008_minimum_swaps_bracket_balancing.cpp
+++ greedy/gfg/008_minimum_swaps_bracket_balancing.cpp
@@ -1,26 +1,33 @@
 #include "../../template.hpp"
 
-void MinSwapsToBalanceBrackets(const string& s) {
-    int left = 0, right = 0, n = s.size(), swaps = 0;
-    for (int i = 0; i < n; ++i) {
-        if (s[i] == '[' and right == 0) {
-            ++left;
+void MinSwapsToBalanceBrackets(string s) {
+    int n = s.size();
+    int left = 0;
+    int right = 0;
+    int ans = 0;
+    for (char ch : s) {
+        if (ch == '[') {
+            if (right == 0) {
+                ++left;
+            }
+            else {
+                ans += right;
+                --right;
+            }
         }
-        else if (s[i] == '[' and right > 0) { // swap
-            swaps += right;
-            --right;
-        }
-        else if (s[i] == ']' and left == 0) {
-            ++right;
-        }
-        else if (s[i] == ']' and left > 0) {
-            --left;
+        else if (ch == ']') {
+            if (left == 0) {
+                ++right;
+            }
+            else {
+                --left;
+            }
         }
     }
-    cout << swaps << '\n';
+    cout << ans << endl;
 }
 
-int main() { TimeMeasure _;
+int main() { TimeMeasure _; __x();
     MinSwapsToBalanceBrackets("]][["); // 3
     MinSwapsToBalanceBrackets("[]][]["); // 2
     MinSwapsToBalanceBrackets("[[][]]"); // 0
