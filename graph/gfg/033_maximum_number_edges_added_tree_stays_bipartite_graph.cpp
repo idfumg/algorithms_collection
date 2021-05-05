b800db5de5b14f18b0bc4683efaca90f5ae924aa
Artem Pushkin <idfumg@gmail.com>
2021-05-05 10:56:39 +0300

    Update graph/gfg/033_maximum_number_edges_added_tree_stays_bipartite_graph.cpp

--- graph/gfg/033_maximum_number_edges_added_tree_stays_bipartite_graph.cpp
+++ graph/gfg/033_maximum_number_edges_added_tree_stays_bipartite_graph.cpp
@@ -2,92 +2,101 @@
 
 using Graph = vvi;
 
-void AddEdge(Graph& graph, int from, int to) {
-    graph[from].push_back(to);
-    graph[to].push_back(from);
+void AddEdge(Graph& graph, int from, int to, int cost = 0) {
+    graph[from - 1].push_back(to - 1);
+    graph[to - 1].push_back(from - 1);
+    // graph.push_back({from, to});
+    // graph[from].push_back({to, cost});
+    // graph[to].push_back({from, cost});
+    // graph.push_back({from, to, cost});
+    // graph[from][to] = cost;
+    // graph[to][from] = cost;
 }
 
-void FindMaxEdgesBFS(Graph& graph) {
-    int n = graph.size();
-    int currentEdges = 0;
-
-    vi color(n, -1);
-    color[1] = 1;
-
-    vi colorCount = {0, 1};
-
-    qi q;
-    q.push(1);
-
-    while (not q.empty()) {
-        int at = q.front(); q.pop();
-        for (int adj : graph[at]) {
-            if (color[at] == 1) {
-                ++currentEdges;
-            }
-            if (color[adj] == -1) {
-                color[adj] = 1 - color[at];
-                ++colorCount[color[adj]];
-                q.push(adj);
-            }
-            else if (color[adj] == color[at]) {
-                cout << "Error! Graph is not a bipartite graph!" << '\n';
-                return;
-            }
-        }
-    }
-    int totalEdges = colorCount[0] * colorCount[1];
-    cout << totalEdges - currentEdges << '\n';
-}
-
-bool dfs(Graph& graph, vi& color, vi& colorCount, int at, int& currentEdges) {
+bool dfs(vvi graph, vi& color, int at) {
     for (int adj : graph[at]) {
-        if (color[at] == 1) {
-            ++currentEdges;
-        }
         if (color[adj] == -1) {
             color[adj] = 1 - color[at];
-            ++colorCount[color[adj]];
-            if (not dfs(graph, color, colorCount, adj, currentEdges)) return false;
+            if (not dfs(graph, color, adj)) return false;
         }
-        else if (color[adj] == color[at]) {
+        else if (color[at] == color[adj]) {
             return false;
         }
     }
     return true;
 }
 
-void FindMaxEdgesDFS(Graph& graph) {
+void FindMaxEdgesDFS(vvi graph) {
     int n = graph.size();
-    int currentEdges = 0;
-
     vi color(n, -1);
-    vi colorCount = {0, 1};
-
-    for (int i = 1; i < n; ++i) {
-        if (color[i] == -1) {
-            color[1] = 1;
-            if (not dfs(graph, color, colorCount, i, currentEdges)) {
-                cout << "Error! Graph is not a bipartite graph!" << '\n';
+    for (int at = 0; at < n; ++at) {
+        if (color[at] == -1) {
+            color[0] = 0;
+            if (not dfs(graph,color, at)) {
+                cout << "Not bipartite" << endl;
                 return;
             }
         }
     }
+    int reds = 0;
+    int blacks = 0;
+    int redsConnections = 0;
+    for (int i = 0; i < n; ++i) {
+        if (color[i] == 0) {
+            ++reds;
+            redsConnections += graph[i].size();
+        }
+        else if (color[i] == 1) {
+            ++blacks;
+        }
+    }
+    cout << reds * blacks - redsConnections << endl;
+}
 
-    int totalEdges = colorCount[0] * colorCount[1];
-    cout << totalEdges - currentEdges << '\n';
+void FindMaxEdgesBFS(vvi graph) {
+    int n = graph.size();
+    vi color(n, -1);
+    color[0] = 0;
+    deque<int> q;
+    q.push_back(0);
+    while (not q.empty()) {
+        int at = q.front(); q.pop_front();
+        for (int adj : graph[at]) {
+            if (color[adj] == -1) {
+                color[adj] = 1 - color[at];
+                q.push_back(adj);
+            }
+            else if (color[adj] == color[at]) {
+                cout << "not a bipartite graph" << endl;
+                return;
+            }
+        }
+    }
+    int reds = 0;
+    int blacks = 0;
+    int redsConnections = 0;
+    for (int i = 0; i < n; ++i) {
+        if (color[i] == 0) {
+            ++reds;
+            redsConnections += graph[i].size();
+        }
+        else if (color[i] == 1) {
+            ++blacks;
+        }
+    }
+    cout << reds * blacks - redsConnections << endl;
 }
 
 int main() { TimeMeasure _;
     {
-        Graph graph(4);
+        Graph graph(3);
         AddEdge(graph, 1, 2);
         AddEdge(graph, 1, 3);
         FindMaxEdgesBFS(graph);
         FindMaxEdgesDFS(graph);
     }
     {
-        Graph graph(6);
+        Graph graph(5);
         AddEdge(graph, 1, 2);
         AddEdge(graph, 1, 3);
         AddEdge(graph, 2, 4);
