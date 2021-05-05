72778d40582fddbe5c5a69fcb64c701f45a243b6
Artem Pushkin <idfumg@gmail.com>
2021-05-05 10:56:39 +0300

    Update graph/gfg/027_shortest_path_reach_one_prime_changing_single_digit_time.cpp

--- graph/gfg/027_shortest_path_reach_one_prime_changing_single_digit_time.cpp
+++ graph/gfg/027_shortest_path_reach_one_prime_changing_single_digit_time.cpp
@@ -2,84 +2,89 @@
 
 using Graph = vvi;
 
-vi GenPrimes(int from, int to) {
-    vi prime(to + 1, true);
+void AddEdge(Graph& graph, int from, int to, int cost = 0) {
+    graph[from].push_back(to);
+    // graph[to].push_back(from);
+    // graph.push_back({from, to});
+    // graph[from].push_back({to, cost});
+    // graph[to].push_back({from, cost});
+    // graph.push_back({from, to, cost});
+    // graph[from][to] = cost;
+    // graph[to][from] = cost;
+}
+
+vi GetPrimes(int from, int to) {
+    vb isprime(to + 1, true);
     for (int p = 2; p * p <= to; ++p) {
-        if (prime[p]) {
+        if (isprime[p]) {
             for (int i = p * p; i <= to; i += p) {
-                prime[i] = false;
+                isprime[i] = false;
             }
         }
     }
     vi primes;
-    for (int p = from; p <= to; ++p) {
-        if (prime[p]) {
-            primes.push_back(p);
+    for (int i = from; i <= to; ++i) {
+        if (isprime[i]) {
+            primes.push_back(i);
         }
     }
     return primes;
 }
 
-bool IsValid(int x, int y) {
+bool IsEdge(int a, int b) {
     int count = 0;
-    while (x > 0 and y > 0) {
-        int remx = x % 10;
-        int remy = y % 10;
-        x /= 10;
-        y /= 10;
-        count += remx != remy ? 1 : 0;
+    for (; a > 0 and b > 0; a /= 10, b /= 10) {
+        count += (a % 10) != (b % 10);
     }
     return count == 1;
 }
 
-Graph GenGraph(const vi& primes) {
-    int n = size(primes);
-    Graph graph(n);
+vvi GetGraph(vi primes) {
+    int n = primes.size();
+    vvi graph(n);
     for (int i = 0; i < n; ++i) {
-        for (int j = i + 1; j < n; ++j) {
-            if (IsValid(primes[i], primes[j])) {
+        for (int j = 1; j < n; ++j) {
+            if (IsEdge(primes[i], primes[j])) {
                 graph[i].push_back(j);
-                graph[j].push_back(i);
             }
         }
     }
     return graph;
 }
 
-int bfs(const Graph& graph, int x, int y, const vi& primes) {
-    int n = size(primes);
-    int from = distance(begin(primes), find(begin(primes), end(primes), x));
+int bfs(int from, int to, vi primes, vvi graph) {
+    int n = graph.size();
 
-    queue<int> q;
-    q.push(from);
+    deque<int> q;
+    q.push_back(0);
 
-    vi dist(n, -1);
-    dist[from] = 0;
+    vb visited(n);
+
+    vi dist(n, INF);
+    dist[0] = 0;
 
     while (not q.empty()) {
-        int at = q.front(); q.pop();
-        if (primes[at] == y) {
-            return dist[at];
-        }
+        int at = q.front(); q.pop_front();
+        visited[at] = true;
         for (int adj : graph[at]) {
-            if (dist[adj] == -1) {
-                q.push(adj);
-                dist[adj] = dist[at] + 1;
+            if (not visited[adj]) {
+                dist[adj] = min(dist[adj], dist[at] + 1);
+                q.push_back(adj);
             }
         }
     }
-    return -1;
+
+    return dist[n - 1];
 }
 
-int MinSteps(int x, int y) {
-    const int from = pow(10, static_cast<int>(log10(x)));
-    const auto primes = GenPrimes(from, y);
-    const auto graph = GenGraph(primes);
-    return bfs(graph, x, y, primes);
+int MinSteps(int a, int b) {
+    vi primes = GetPrimes(a, b);
+    vvi graph = GetGraph(primes);
+    return bfs(a, b, primes, graph);
 }
 
 int main() { TimeMeasure _;
-    cout << MinSteps(1033, 8179) << endl;
-    cout << MinSteps(1373, 8017) << endl;
-    cout << MinSteps(1033, 1033) << endl;
+    cout << MinSteps(1033, 8179) << endl; // 6
+    cout << MinSteps(1373, 8017) << endl; // 7
+    cout << MinSteps(1033, 1033) << endl; // 0
 }
