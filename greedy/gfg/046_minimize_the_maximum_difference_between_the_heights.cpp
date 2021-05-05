deedc27144a8d0a358bdc3231b8128bac8cdcde9
Artem Pushkin <idfumg@gmail.com>
2021-05-05 10:56:39 +0300

    Update greedy/gfg/046_minimize_the_maximum_difference_between_the_heights.cpp

--- greedy/gfg/046_minimize_the_maximum_difference_between_the_heights.cpp
+++ greedy/gfg/046_minimize_the_maximum_difference_between_the_heights.cpp
@@ -19,20 +19,26 @@ int rec(vi arr, int k) {
 }
 
 int rec2(vi arr, int k, int n, int mini, int maxi) {
-    if (n == 0) {
-        if (mini >= 0 and maxi >= 0) {
-            return abs(maxi - mini);
-        }
-        return INF;
-    }
-
-    return min(
-        rec2(arr, k, n - 1, min(mini, arr[n - 1] + k), max(maxi, arr[n - 1] + k)),
-        rec2(arr, k, n - 1, min(mini, arr[n - 1] - k), max(maxi, arr[n - 1] - k)));
+    if (mini < 0) return INF;
+    if (n == 0) return maxi - mini;
+    return min(rec2(arr, k, n - 1, min(mini, arr[n - 1] + k), max(maxi, arr[n - 1] + k)),
+               rec2(arr, k, n - 1, min(mini, arr[n - 1] - k), max(maxi, arr[n - 1] - k)));
 }
 
 int rec2(vi arr, int k) {
     return rec2(arr, k, arr.size(), INF, -INF);
+}
+
+int tab(vi arr, int k) {
+    sort(arr.begin(), arr.end());
+    int n = arr.size();
+    int ans = arr[n - 1] - arr[0];
+    for (int i = 0; i < n - 1; ++i) {
+        int mini = min(arr[0] + k, arr[i + 1] - k);
+        int maxi = max(arr[i] + k, arr[n - 1] - k);
+        ans = min(ans, maxi - mini);
+    }
+    return ans;
 }
 
 int main() { TimeMeasure _; __x();
@@ -56,4 +62,11 @@ int main() { TimeMeasure _; __x();
     cout << rec2(arr4, k4) << '\n'; // 2
     cout << rec2(arr5, k5) << '\n'; // 5
     cout << rec2(arr6, k6) << '\n'; // 2
+    cout << '\n';
+    cout << tab(arr1, k1) << '\n'; // 5
+    cout << tab(arr2, k2) << '\n'; // 8
+    cout << tab(arr3, k3) << '\n'; // 2
+    cout << tab(arr4, k4) << '\n'; // 2
+    cout << tab(arr5, k5) << '\n'; // 5
+    cout << tab(arr6, k6) << '\n'; // 2
 }
