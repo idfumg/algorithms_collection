8bd0b187a20223079ddd76c10f73b50bee35a973
Artem Pushkin <idfumg@gmail.com>
2021-05-05 10:56:39 +0300

    Update graph/gfg/061_maximum_edges_can_added_dag_remains_dag.cpp

--- graph/gfg/061_maximum_edges_can_added_dag_remains_dag.cpp
+++ graph/gfg/061_maximum_edges_can_added_dag_remains_dag.cpp
@@ -33,13 +33,40 @@ vi KhansTopoSort(Graph& graph) {
     return ordering;
 }
 
-int MaximumEdgesToAdd(Graph& graph) {
+vi KhanTopoSort(vvi graph) {
     int n = graph.size();
-    vi ordering = KhansTopoSort(graph);
-    int count = 0;
+    vi indegree(n);
     for (int i = 0; i < n; ++i) {
-        vb visited(n);
+        for (int adj : graph[i]) {
+            ++indegree[adj];
+        }
+    }
+    deque<int> q;
+    for (int i = 0; i < n; ++i) {
+        if (indegree[i] == 0) {
+            q.push_back(i);
+        }
+    }
+    vi ordering;
+    while (not q.empty()) {
+        int at = q.front(); q.pop_front();
+        ordering.push_back(at);
+        for (int adj : graph[at]) {
+            if (--indegree[adj] == 0) {
+                q.push_back(adj);
+            }
+        }
+    }
+    return ordering;
+}
+
+int MaximumEdgesToAdd(vvi graph) {
+    int n = graph.size();
+    int count = 0;
+    vi ordering = KhanTopoSort(graph);
+    for (int i = 0; i < n; ++i) {
         int at = ordering[i];
+        vb visited(n);
         for (int adj : graph[at]) {
             visited[adj] = true;
         }
@@ -52,19 +79,18 @@ int MaximumEdgesToAdd(Graph& graph) {
     return count;
 }
 
-void MaximumEdgesToPrint(Graph& graph) {
+void MaximumEdgesToPrint(vvi graph) {
     int n = graph.size();
-    vi ordering = KhansTopoSort(graph);
+    vi ordering = KhanTopoSort(graph);
     for (int i = 0; i < n; ++i) {
-        int idx = ordering[i];
+        int at = ordering[i];
         vb visited(n);
-        for (int adj : graph[idx]) {
+        for (int adj : graph[at]) {
             visited[adj] = true;
         }
         for (int j = i + 1; j < n; ++j) {
-            int nextIdx = ordering[j];
-            if (not visited[nextIdx]) {
-                cout << idx << '-' << nextIdx << '\n';
+            if (not visited[ordering[j]]) {
+                cout << at << '-' << ordering[j] << endl;
             }
         }
     }
