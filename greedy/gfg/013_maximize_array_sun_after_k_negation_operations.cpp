21fec8b9e8e1c1d798661ebd7962f60e65e7223a
Artem Pushkin <idfumg@gmail.com>
2021-05-05 10:56:39 +0300

    Update greedy/gfg/013_maximize_array_sun_after_k_negation_operations.cpp

--- greedy/gfg/013_maximize_array_sun_after_k_negation_operations.cpp
+++ greedy/gfg/013_maximize_array_sun_after_k_negation_operations.cpp
@@ -2,36 +2,66 @@
 
 int MaximizeArraySum(vi arr, int k) {
     int n = arr.size();
-    int ans = 0;
     sort(arr.begin(), arr.end());
+
     int i = 0;
-    int lastNegative = -1;
-    while (i < n and arr[i] < 0) {
+    while (arr[i] < 0 and k-- > 0) {
+        arr[i++] *= -1;
+    }
+
+    if (arr[i] != 0 and k & 1) {
         arr[i] *= -1;
-        ++i;
-        --k;
-        lastNegative = i;
     }
-    if (arr[i] == 0) {
-        k = 0;
-    }
-    else if (k % 2 == 0) {
-        k = 0;
-    }
-    else {
-        if (lastNegative != -1 and arr[lastNegative] < arr[i]) {
-            arr[lastNegative] *= -1;
-        }
-        else {
-            arr[i] *= -1;
-        }
-    }
+
     return accumulate(arr.begin(), arr.end(), 0);
 }
 
-int main() { TimeMeasure _;
+int MaximizeArraySum2(vi arr, int k) {
+    int n = arr.size();
+    int posCount = 0;
+    int posSum = 0;
+    int posMin = INF;
+    int zeroCount = 0;
+    int negSum = 0;
+    for (int i = 0; i < n; ++i) {
+        if (arr[i] < 0 and k > 0) {
+            arr[i] *= -1;
+            --k;
+            ++posCount;
+            posSum += arr[i];
+            posMin = min(posMin, arr[i]);
+        }
+        else if (arr[i] < 0) {
+            negSum += arr[i];
+        }
+        else if (arr[i] == 0) {
+            ++zeroCount;
+        }
+        else {
+            ++posCount;
+            posSum += arr[i];
+            posMin = min(posMin, arr[i]);
+        }
+    }
+    if (k > 0 and zeroCount > 0) {
+        k = 0;
+    }
+    if (k > 0 and posCount > 0) {
+        if (k & 1) {
+            posSum -= 2 * posMin;
+            k = 0;
+        }
+    }
+
+    return negSum + posSum;
+}
+
+int main() { TimeMeasure _; __x();
     vi arr1 = {-2, 0, 5, -1, 2}; // 10
     vi arr2 = {9, 8, 8, 5}; // 20
     cout << MaximizeArraySum(arr1, 4) << '\n';
     cout << MaximizeArraySum(arr2, 3) << '\n';
+    cout << endl;
+    cout << MaximizeArraySum2(arr1, 4) << '\n';
+    cout << MaximizeArraySum2(arr2, 3) << '\n';
 }
