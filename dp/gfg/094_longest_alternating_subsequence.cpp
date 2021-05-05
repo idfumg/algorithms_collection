d1c9fe40e9995848f64fb01feee7117476302dda
Artem Pushkin <idfumg@gmail.com>
2021-05-05 10:56:38 +0300

    Update dp/gfg/094_longest_alternating_subsequence.cpp

--- dp/gfg/094_longest_alternating_subsequence.cpp
+++ dp/gfg/094_longest_alternating_subsequence.cpp
@@ -1,53 +1,89 @@
 #include "../../template.hpp"
 
-int rec(vi arr, int n, bool increase, int total) {
-    if (n == 0) {
-        return total;
+int rec(vi arr, int i, int prev, bool isincr) {
+    int n = arr.size();
+    if (i == n) {
+        return 0;
     }
-    if ((increase and arr[n - 1] > arr[n]) or (not increase and arr[n - 1] < arr[n])) {
-        return max(rec(arr, n - 1, not increase, total + 1),
-                   rec(arr, n - 1, increase, total));
+    if (isincr and arr[i] > arr[prev]) {
+        return max(1 + rec(arr, i + 1, i, false), rec(arr, i + 1, prev, isincr));
     }
-    return rec(arr, n - 1, increase, total);
+    else if (not isincr and arr[i] < arr[prev]) {
+        return max(1 + rec(arr, i + 1, i, true), rec(arr, i + 1, prev, isincr));
+    }
+    return rec(arr, i + 1, prev, isincr);
 }
 
 int rec(vi arr) {
-    int n = arr.size();
-    return max(rec(arr, n - 1, true, 1), rec(arr, n - 1, false, 1));
-}
-
-int rec2(vi arr, int n, bool increase) {
-    if (n == 0) {
-        return 0;
-    }
-    if ((increase and arr[n - 1] > arr[n]) or (not increase and arr[n - 1] < arr[n])) {
-        return max(rec2(arr, n - 1, not increase) + 1,
-                   rec2(arr, n - 1, increase));
-    }
-    return rec2(arr, n - 1, increase);
-}
-
-int rec2(vi arr) {
-    int n = arr.size();
-    return max(rec2(arr, n - 1, true), rec2(arr, n - 1, false)) + 1;
+    return max(rec(arr, 1, 0, false), rec(arr, 1, 0, true)) + 1;
 }
 
 int tab(vi arr) {
     int n = arr.size();
-    vvi dp(n + 1, vi(2));
-    for (int i = 1; i <= n; ++i) {
-        for (bool increase : {true, false}) {
-            if ((increase and arr[i - 1] > arr[i]) or (not increase and arr[i - 1] < arr[i])) {
-                dp[i][increase] = max(dp[i - 1][not increase] + 1,
-                                      dp[i - 1][increase]);
-            }
-            else {
-                dp[i][increase] = dp[i - 1][increase];
+    vvvi dp(n + 2, vvi(n + 2, vi(2)));
+    for (int i = n - 1; i >= 0; --i) {
+        for (int j = n - 1; j >= 0; --j) {
+            for (int k : {0, 1}) {
+                if (i == n) {
+                    dp[i][j][k] = 0;
+                }
+                else if (k == 1 and arr[i] > arr[j]) {
+                    dp[i][j][k] = max(1 + dp[i + 1][i][0], dp[i + 1][j][1]);
+                }
+                else if (k == 0 and arr[i] < arr[j]) {
+                    dp[i][j][k] = max(1 + dp[i + 1][i][1], dp[i + 1][j][0]);
+                }
+                else {
+                    dp[i][j][k] = dp[i + 1][j][k];
+                }
             }
         }
     }
-    return max(dp[n - 1][true], dp[n - 1][false]) + 1;
+    return 1 + max(dp[1][0][0], dp[1][0][1]);
 }
+
+int rec2(vi arr, int n, int prev, int down) {
+    if (n == 0) return 0;
+    if (down) {
+        if (arr[n - 1] < arr[prev - 1]) {
+            return max(rec2(arr, n - 1, n, !down) + 1, rec2(arr, n - 1, prev, down));
+        }
+        else {
+            return rec2(arr, n - 1, prev, down);
+        }
+    }
+    if (arr[n - 1] > arr[prev - 1]) {
+        return max(rec2(arr, n - 1, n, !down) + 1, rec2(arr, n - 1, prev, down));
+    }
+    else {
+        return rec2(arr, n - 1, prev, down);
+    }
+}
+
+int rec2(vi arr) {
+    return max(rec2(arr, arr.size() - 1, arr.size(), 0), rec2(arr, arr.size() - 1, arr.size(), 1)) + 1;
+}
+
+int tab2(vi arr) {
+    int n = arr.size();
+    vvvi dp(n + 1, vvi(n + 1, vi(2)));
+    for (int i = 1; i <= n; ++i) {
+        for (int j = 1; j <= n; ++j) {
+            for (int k : {0, 1}) {
+                if (k) {
+                    if (arr[i - 1] < arr[j - 1]) dp[i][j][k] = max(dp[i - 1][i][!k] + 1, dp[i - 1][j][k]);
+                    else dp[i][j][k] = dp[i - 1][j][k];
+                }
+                else {
+                    if (arr[i - 1] > arr[j - 1]) dp[i][j][k] = max(dp[i - 1][i][!k] + 1, dp[i - 1][j][k]);
+                    else dp[i][j][k] = dp[i - 1][j][k];
+                }
+            }
+        }
+    }
+    return max(dp[n - 1][n][0], dp[n - 1][n][1]) + 1;
+}
+
 
 int main() { TimeMeasure _; __x();
     vi arr1 = {1, 5, 4};
@@ -62,15 +98,21 @@ int main() { TimeMeasure _; __x();
     cout << rec(arr4) << endl; // 6
     cout << rec(arr5) << endl; // 3
     cout << '\n';
+    cout << tab(arr1) << endl; // 3
+    cout << tab(arr2) << endl; // 2
+    cout << tab(arr3) << endl; // 6
+    cout << tab(arr4) << endl; // 6
+    cout << tab(arr5) << endl; // 3
+    cout << '\n';
     cout << rec2(arr1) << endl; // 3
     cout << rec2(arr2) << endl; // 2
     cout << rec2(arr3) << endl; // 6
     cout << rec2(arr4) << endl; // 6
     cout << rec2(arr5) << endl; // 3
     cout << '\n';
-    cout << tab(arr1) << endl; // 3
-    cout << tab(arr2) << endl; // 2
-    cout << tab(arr3) << endl; // 6
-    cout << tab(arr4) << endl; // 6
-    cout << tab(arr5) << endl; // 3
+    cout << tab2(arr1) << endl; // 3
+    cout << tab2(arr2) << endl; // 2
+    cout << tab2(arr3) << endl; // 6
+    cout << tab2(arr4) << endl; // 6
+    cout << tab2(arr5) << endl; // 3
 }
