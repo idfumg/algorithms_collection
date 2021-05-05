ff1843c6c4338ed4fed545c710a991ad40164a45
Artem Pushkin <idfumg@gmail.com>
2021-05-05 10:56:39 +0300

    Update dp/gfg/158_count_sum_of_digits_in_numbers_from_1_to_n.cpp

--- dp/gfg/158_count_sum_of_digits_in_numbers_from_1_to_n.cpp
+++ dp/gfg/158_count_sum_of_digits_in_numbers_from_1_to_n.cpp
@@ -1,34 +1,45 @@
 #include "../../template.hpp"
 
-int SumOfDigits(int n) {
-    int res = 0;
-    while (n > 0) {
-        res += n % 10;
-        n /= 10;
+int naive(int num) {
+    int ans = 0;
+    for (int i = 1; i <= num; ++i) {
+        for (int n = i; n > 0; n /= 10) {
+            ans += n % 10;
+        }
     }
-    return res;
+    return ans;
 }
 
-int naive(int n) {
-    int res = 0;
-    for (int i = 1; i <= n; ++i) {
-        res += SumOfDigits(i);
-    }
-    return res;
+int rec_(int n) {
+    if (n < 10) return n;
+    return n % 10 + rec_(n / 10);
+}
+
+int rec(int n) {
+    if (n == 0) return 0;
+    return rec_(n) + rec(n - 1);
 }
 
 int tab(int n) {
     vi dp(n + 1);
     for (int i = 1; i <= n; ++i) {
-        dp[i] = dp[i / 10] + i % 10;
+        if (i < 10) dp[i] = i;
+        else dp[i] = i % 10 + dp[i / 10];
     }
-    return accumulate(dp.begin(), dp.end(), 0);
+    int ans = 0;
+    for (int i = 1; i <= n; ++i) {
+        ans += dp[i];
+    }
+    return ans;
 }
 
-int main() { TimeMeasure _; __x();
+int main() { // TimeMeasure _; __x();
     cout << naive(5) << endl; // 15
     cout << naive(12) << endl; // 51
     cout << naive(328) << endl; // 3241
+    cout << rec(5) << endl; // 15
+    cout << rec(12) << endl; // 51
+    cout << rec(328) << endl; // 3241
     cout << tab(5) << endl; // 15
     cout << tab(12) << endl; // 51
     cout << tab(328) << endl; // 3241
