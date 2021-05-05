1e4328db877ca077dc5260c699f603a17d8b8eb5
Artem Pushkin <idfumg@gmail.com>
2021-05-05 10:56:39 +0300

    Update math/gfg/019_queries_gcd_numbers_array_except_elements_given_range.cpp

--- math/gfg/019_queries_gcd_numbers_array_except_elements_given_range.cpp
+++ math/gfg/019_queries_gcd_numbers_array_except_elements_given_range.cpp
@@ -3,19 +3,19 @@
 int GcdInRange(vi arr, int a, int b) {
     int n = arr.size();
     vi prefix(n + 1);
-    vi suffix(n + 1);
-    for (int i = 1; i <= n; ++i) {
+    vi suffix(n + 2);
+    for (int i = 1; i < n; ++i) {
         prefix[i] = gcd(prefix[i - 1], arr[i - 1]);
     }
-    for (int i = n; i >= 0; --i) {
+    for (int i = n; i >= 1; --i) {
         suffix[i] = gcd(suffix[i + 1], arr[i - 1]);
     }
     if (a == 0) return suffix[2];
-    if (b == n - 1) return prefix[n - 1];
+    if (b == n - 1) return prefix[a + 1];
     return gcd(prefix[a], suffix[b]);
 }
 
-int main() { TimeMeasure _; __x();
+int main() { TimeMeasure _;
     cout << GcdInRange({2, 6, 9}, 0, 0) << endl; // 3
     cout << GcdInRange({2, 6, 9}, 1, 1) << endl; // 1
     cout << GcdInRange({2, 6, 9}, 1, 2) << endl; // 2
