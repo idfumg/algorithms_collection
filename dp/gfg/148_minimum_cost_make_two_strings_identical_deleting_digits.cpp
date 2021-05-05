af13a0590d5d6fa9d48c1d0112347aeef2d38efe
Artem Pushkin <idfumg@gmail.com>
2021-05-05 10:56:39 +0300

    Update dp/gfg/148_minimum_cost_make_two_strings_identical_deleting_digits.cpp

--- dp/gfg/148_minimum_cost_make_two_strings_identical_deleting_digits.cpp
+++ dp/gfg/148_minimum_cost_make_two_strings_identical_deleting_digits.cpp
@@ -62,6 +62,33 @@ int tab2(const string& a, const string& b) { // we can just find answer fast her
     return total - dp[m][n];
 }
 
+int rec2(string a, string b, int m, int n) {
+    if (m == 0) return reduce(b.begin(), b.begin() + n, 0) - n * '0';
+    if (n == 0) return reduce(a.begin(), a.begin() + m, 0) - m * '0';
+    if (a[m - 1] == b[n - 1]) return rec2(a, b, m - 1, n - 1);
+    return min(rec2(a, b, m - 1, n) + a[m - 1] - '0',
+               rec2(a, b, m, n - 1) + b[n - 1] - '0');
+}
+
+int rec2(string a, string b) {
+    return rec2(a, b, a.size(), b.size());
+}
+
+int tab3(string a, string b) {
+    int m = a.size(), n = b.size();
+    vvi dp(m + 1, vi(n + 1));
+    for (int i = 0; i <= m; ++i) {
+        for (int j = 0; j <= n; ++j) {
+            if (i == 0) dp[i][j] = reduce(b.begin(), b.begin() + j, 0) - j * '0';
+            else if (j == 0) dp[i][j] = reduce(a.begin(), a.begin() + i, 0) - i * '0';
+            else if (a[i - 1] == b[j - 1]) dp[i][j] = dp[i - 1][j - 1];
+            else dp[i][j] = min(dp[i - 1][j] + a[i - 1] - '0',
+                                dp[i][j - 1] + b[j - 1] - '0');
+        }
+    }
+    return dp[m][n];
+}
+
 int main() { TimeMeasure _; __x();
     cout << rec("3759", "9350") << endl; // 23
     cout << rec("3198", "98") << endl; // 4
@@ -71,4 +98,10 @@ int main() { TimeMeasure _; __x();
     cout << '\n';
     cout << tab2("3759", "9350") << endl; // 23
     cout << tab2("3198", "98") << endl; // 4
+    cout << '\n';
+    cout << rec2("3759", "9350") << endl; // 23
+    cout << rec2("3198", "98") << endl; // 4
+    cout << '\n';
+    cout << tab3("3759", "9350") << endl; // 23
+    cout << tab3("3198", "98") << endl; // 4
 }
