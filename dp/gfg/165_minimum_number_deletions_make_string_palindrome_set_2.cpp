8512a61e61a43f6e35d78999140653dbcec9393e
Artem Pushkin <idfumg@gmail.com>
2021-05-05 10:56:39 +0300

    Update dp/gfg/165_minimum_number_deletions_make_string_palindrome_set_2.cpp

--- dp/gfg/165_minimum_number_deletions_make_string_palindrome_set_2.cpp
+++ dp/gfg/165_minimum_number_deletions_make_string_palindrome_set_2.cpp
@@ -1,21 +1,28 @@
 #include "../../template.hpp"
 
-int tab(const string& a) {
-    string b = a;
-    reverse(b);
-    int m = a.size(), n = b.size();
-    vvi dp(m + 1, vi(n + 1));
-    for (int i = 1; i <= m; ++i) {
+int tab(string s) {
+    int n = s.size();
+    vvi dp(n + 2, vi(n + 1));
+    for (int i = n; i >= 1; --i) {
         for (int j = 1; j <= n; ++j) {
-            if (a[i - 1] == b[j - 1]) {
-                dp[i][j] = 1 + dp[i - 1][j - 1];
+            if (i > j) {
+                dp[i][j] = INF;
+            }
+            else if (i == j) {
+                dp[i][j] = 0;
+            }
+            else if (j - i == 1) {
+                dp[i][j] = s[i - 1] != s[j - 1];
+            }
+            else if (s[i - 1] == s[j - 1]) {
+                dp[i][j] = dp[i + 1][j - 1];
             }
             else {
-                dp[i][j] = max({dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1]});
+                dp[i][j] = min(dp[i + 1][j], dp[i][j - 1]) + 1;
             }
         }
     }
-    return n - dp[m][n];
+    return dp[1][n];
 }
 
 int main() { TimeMeasure _; __x();
