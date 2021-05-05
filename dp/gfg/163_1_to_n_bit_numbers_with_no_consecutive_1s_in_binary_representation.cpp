6c5ea4734307e676d12239949df070a226952737
Artem Pushkin <idfumg@gmail.com>
2021-05-05 10:56:39 +0300

    Update dp/gfg/163_1_to_n_bit_numbers_with_no_consecutive_1s_in_binary_representation.cpp

--- dp/gfg/163_1_to_n_bit_numbers_with_no_consecutive_1s_in_binary_representation.cpp
+++ dp/gfg/163_1_to_n_bit_numbers_with_no_consecutive_1s_in_binary_representation.cpp
@@ -1,5 +1,25 @@
 #include "../../template.hpp"
 
+bool NoConsecutiveOnes(int n) {
+    int prev = n & 1;
+    for (n >>= 1; n > 0; n >>= 1) {
+        int curr = n & 1;
+        if (curr == 1 and prev == 1) return false;
+        prev = curr;
+    }
+    return true;
+}
+
+vi naive(int n) {
+    vi ans;
+    for (int i = 0; i < (1 << n); ++i) {
+        if (NoConsecutiveOnes(i)) {
+            ans.push_back(i);
+        }
+    }
+    return ans;
+}
+
 int with1(int n);
 
 int with0(int n) {
@@ -16,81 +36,44 @@ int rec_count(int n) {
     return with0(n) + with1(n);
 }
 
-bool IsNonConsequentOnes(int n) {
-    int prev = 0, digits = static_cast<int>(log2(n));
-    for (int i = 1 << 0; i <= (1 << digits); prev = i, i <<= 1) {
-        if ((n & prev) and (n & i)) {
-            return false;
-        }
-    }
-    return true;
+int rec_count2(int n, int prev) {
+    if (n == 0) return 1;
+    if (prev == 0) return rec_count2(n - 1, 1) + rec_count2(n - 1, 0);
+    return rec_count2(n - 1, 0);
 }
 
-vi naive(int n) {
-    vi res;
-    for (int i = 0; i < (1 << n); ++i) {
-        if (IsNonConsequentOnes(i)) {
-            res.push_back(i);
-        }
-    }
-    return res;
+int rec_count2(int n) {
+    return rec_count2(n, 0);
 }
 
-void rec(int n, vi& res, int prev, si& elems) {
+int rec(int n, int prev, int num, vi& ans) {
     if (n == 0) {
-        int num = 0;
-        for (int i = size(res) - 1; i >= 0; --i) {
-            if (res[i] == 1) {
-                num += pow(2, size(res) - i - 1);
-            }
-        }
-        elems.insert(num);
-        return;
-    }
-    for (int i : {0, 1}) {
-        if (prev == 1 and i == 1) continue;
-        res[n - 1] = i;
-        rec(n - 1, res, i, elems);
-    }
-}
-
-si rec(int n) {
-    vi res(n);
-    si elems;
-    for (int i : {0, 1}) {
-        res[n - 1] = i;
-        rec(n - 1, res, i, elems);
-    }
-    return elems;
-}
-
-void rec_elems(int init, int n, int prev, int result) {
-    if (n == 0) {
-        cout << result << ' ';
-        return;
+        ans.push_back(num);
+        return 1;
     }
     if (prev == 0) {
-        rec_elems(init, n - 1, 0, result);
-        rec_elems(init, n - 1, 1, result + (1 << (init - n)));
-        return;
+        return rec(n - 1, 1, (num << 1) + 1, ans) + rec(n - 1, 0, num << 1, ans);
     }
-    rec_elems(init, n - 1, 0, result);
+    return rec(n - 1, 0, num << 1, ans);
 }
 
-void rec_elems(int n) {
-    return rec_elems(n, n, 0, 0);
+vi rec(int n) {
+    vi ans;
+    rec(n, 0, 0, ans);
+    reverse(ans.begin(), ans.end());
+    return ans;
 }
 
-int main() { TimeMeasure _; __x();
-    cout << rec_count(4) << endl; // 8
-    cout << rec_count(3) << endl; // 5
-
+int main() { // TimeMeasure _; __x();
     cout << naive(4) << endl; // 0 1 2 4 5 8 9 10
     cout << naive(3) << endl; // 0 1 2 4 5
 
+    cout << rec_count(4) << endl; // 8
+    cout << rec_count(3) << endl; // 5
+
+    cout << rec_count2(4) << endl; // 8
+    cout << rec_count2(3) << endl; // 5
+
     cout << rec(4) << endl; // 0 1 2 4 5 8 9 10
     cout << rec(3) << endl; // 0 1 2 4 5
-
-    rec_elems(4); cout << endl; // 0 1 2 4 5 8 9 10
-    rec_elems(3); cout << endl; // 0 1 2 4 5
 }
