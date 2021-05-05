553c6b6b55188b369b1284be6735b1a14f817e18
Artem Pushkin <idfumg@gmail.com>
2021-05-05 10:56:39 +0300

    Update greedy/gfg/050_largest_palindromic_number_permuting_digits.cpp

--- greedy/gfg/050_largest_palindromic_number_permuting_digits.cpp
+++ greedy/gfg/050_largest_palindromic_number_permuting_digits.cpp
@@ -1,84 +1,39 @@
 #include "../../template.hpp"
 
-void GenPalindrome(int n) {
-    vi freq(10);
-    while (n > 0) {
-        ++freq[n % 10];
-        n /= 10;
+void GenPalindrome(int num) {
+    vi arr(10);
+    for (int n = num; n > 0; n /= 10) {
+        ++arr[n % 10];
     }
-
-    int odd = 0, oddNumber = -1;
-    for (int i = 0; i <= 9; ++i) {
-        if (freq[i] % 2 != 0) {
-            ++odd;
-            oddNumber = i;
-        }
-    }
-
-    if (odd > 1) {
-        cout << "Palindrome cannot be formed" << '\n';
-        return;
-    }
-
-    string ans;
-    for (int i = 9; i >= 0; --i) {
-        if (freq[i] > 0) {
-            int count = freq[i] / 2;
-            ans += string(count, i + '0');
-        }
-    }
-
-    if (oddNumber != -1) {
-        cout << ans << oddNumber;
-    }
-    else {
-        cout << ans;
-    }
-    reverse(ans);
-    cout << ans << '\n';
-}
-
-void GenPalindrome2(int n) {
-    vi freq(10);
-    while (n > 0) {
-        ++freq[n % 10];
-        n /= 10;
-    }
-
-    int ans = 0, odd = 0, oddNumber = -1;
-    for (int i = 9; i >= 0; --i) {
-        if (freq[i] == 0) {
-            continue;
-        }
-        else if (freq[i] & 1) {
-            ++odd;
-            oddNumber = i;
-        }
-        else {
-            int count = freq[i] / 2;
-            while (count-- > 0) {
-                ans = ans == 0 ? i : ans * 10 + i;
+    int odd = -1;
+    for (int i = 0; i < 10; ++i) {
+        if (arr[i] & 1) {
+            if (odd != -1) {
+                cout << -1 << endl;
+                return;
+            }
+            else {
+                odd = arr[i];
+                --arr[i];
             }
         }
     }
-
-    if (odd > 1) {
-        cout << "Palindrome cannot be formed" << '\n';
-        return;
+    int ans = 0;
+    for (int i = 9; i >= 0; --i) {
+        for (int count = arr[i] / 2; count > 0; --count) {
+            ans = ans * 10 + i;
+            --arr[i];
+        }
     }
-
-    int rev = 0, temp = ans;
-    while (temp > 0) {
-        rev = rev == 0 ? temp % 10 : rev * 10 + temp % 10;
-        temp /= 10;
+    if (odd != -1) {
+        ans = ans * 10 + odd;
     }
-
-    if (oddNumber != -1) {
-        cout << ans << oddNumber << rev << '\n';
+    for (int i = 0; i < 10; ++i) {
+        for (int count = arr[i]; count > 0; --count) {
+            ans = ans * 10 + i;
+        }
     }
-    else {
-        cout << ans << rev << '\n';
-    }
+    cout << ans << endl;
 }
 
 int main() { TimeMeasure _; __x();
@@ -86,8 +41,4 @@ int main() { TimeMeasure _; __x();
     GenPalindrome(313551); // 531135
     GenPalindrome(331); // 313
     GenPalindrome(3444); // Palindrome cannot be formed
-    GenPalindrome2(35135515); // 55311355
-    GenPalindrome2(313551); // 531135
-    GenPalindrome2(331); // 313
-    GenPalindrome2(3444); // Palindrome cannot be formed
 }
