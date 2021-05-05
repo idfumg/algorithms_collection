fef378d799761933b7a9b2d7ba8ac60da7c33cf1
Artem Pushkin <idfumg@gmail.com>
2021-05-05 10:56:38 +0300

    Update arrays/gfg/054_find_triplets_array_whose_sum_equal_zero.cpp

--- arrays/gfg/054_find_triplets_array_whose_sum_equal_zero.cpp
+++ arrays/gfg/054_find_triplets_array_whose_sum_equal_zero.cpp
@@ -1,6 +1,21 @@
 #include "../../template.hpp"
 
-void FindTriplets(vi arr) {
+void FindTriplets1(vi arr) {
+    int n = arr.size();
+    for (int i = 0; i < n; ++i) {
+        for (int j = i + 1; j < n; ++j) {
+            for (int k = j + 1; k < n; ++k) {
+                if (arr[i] + arr[j] + arr[k] == 0) {
+                    cout << arr[i] << ' ' << arr[j] << ' ' << arr[k] << endl;
+                }
+            }
+        }
+    }
+    cout << endl;
+}
+
+
+void FindTriplets2(vi arr) {
     int n = arr.size();
     int total = 0;
     sort(arr.begin(), arr.end());
@@ -20,7 +35,27 @@ void FindTriplets(vi arr) {
     cout << endl;
 }
 
+void FindTriplets3(vi arr) {
+    int n = arr.size();
+    sort(arr.begin(), arr.end());
+    for (int i = 0; i < n; ++i) {
+        for (int j = i + 1; j < n; ++j) {
+            int sum = arr[i] + arr[j];
+            if (binary_search(arr.begin() + j + 1, arr.end(), -sum)) {
+                cout << arr[i] << ' ' << arr[j] << ' ' << -sum << endl;
+            }
+        }
+    }
+    cout << endl;
+}
+
 int main() { TimeMeasure _;
-    FindTriplets({0, -1, 2, -3, 1});
-    FindTriplets({1, -2, 1, 0, 5});
+    FindTriplets1({0, -1, 2, -3, 1}); // O(n^3)
+    FindTriplets1({1, -2, 1, 0, 5});
+    cout << endl;
+    FindTriplets2({0, -1, 2, -3, 1}); // O(n + n^2)
+    FindTriplets2({1, -2, 1, 0, 5});
+    cout << endl;
+    FindTriplets3({0, -1, 2, -3, 1}); // O(logn + n^2)
+    FindTriplets3({1, -2, 1, 0, 5});
 }
