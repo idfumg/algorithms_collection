8f009d0c331e0dbe483d51563c98b2995bd68558
Artem Pushkin <idfumg@gmail.com>
2021-05-05 10:56:38 +0300

    Update dp/gfg/097_count_distinct_subsequences.cpp

--- dp/gfg/097_count_distinct_subsequences.cpp
+++ dp/gfg/097_count_distinct_subsequences.cpp
@@ -1,58 +1,54 @@
 #include "../../template.hpp"
 
-void naive(string s, unordered_set<string>& ans, string& current, int from) {
-    int n = s.size();
-    if (from == n) {
-        ans.insert(current);
-        return;
-    }
-    for (int i = from; i < n; ++i) {
-        naive(s, ans, current, i + 1);
-        current.push_back(s[i]);
-        naive(s, ans, current, i + 1);
-        current.pop_back();
-    }
-}
-
 int naive(string s) {
     unordered_set<string> ans;
-    string current;
-    naive(s, ans, current, 0);
+    int n = s.size();
+    for (int i = 0; i < (1 << n); ++i) {
+        string temp;
+        for (int j = 0; j < n; ++j) {
+            if (i & (1 << j)) {
+                temp += s[j];
+            }
+        }
+        ans.insert(temp);
+    }
     return ans.size();
 }
 
-int naive2(string s) {
-    int n = s.size();
-    unordered_set<string> tab;
-    for (int i = 0; i < (1 << n); ++i) {
-        string seq;
-        for (int j = 0; j < n; ++j) {
-            if (i & (1 << j)) {
-                seq += s[j];
-            }
-        }
-        tab.insert(seq);
+void naive2(string s, int n, unordered_set<string>& ans, string& temp) {
+    if (n == 0) {
+        ans.insert(temp);
+        return;
     }
-    return tab.size();
+    temp.push_back(s[n - 1]);
+    naive2(s, n - 1, ans, temp);
+    temp.pop_back();
+    naive2(s, n - 1, ans, temp);
+}
+
+int naive2(string s) {
+    unordered_set<string> ans;
+    string temp;
+    naive2(s, s.size(), ans, temp);
+    return ans.size();
 }
 
 int tab(string s) {
     int n = s.size();
-    vi dp(n + 1, 1);
-    vi last(27, -1);
+    vi visited(256, -1);
+    vi dp(n + 1);
+    dp[0] = 1;
     for (int i = 1; i <= n; ++i) {
-        dp[i] = 2 * dp[i - 1];
-
-        int ch = s[i - 1] - 'a';
-        if (last[ch] != -1) {
-            dp[i] -= dp[last[ch] - 1];
+        dp[i] = dp[i - 1] * 2;
+        if (visited[s[i - 1] - '0'] != -1) {
+            dp[i] -= dp[visited[s[i - 1] - '0'] - 1];
         }
-        last[ch] = i;
+        visited[s[i - 1] - '0'] = i;
     }
     return dp[n];
 }
 
-int main() { TimeMeasure _; __x();
+int main() { // TimeMeasure _; __x();
     const string s1 = "gfg"; // 7
     const string s2 = "ggg"; // 4
 
