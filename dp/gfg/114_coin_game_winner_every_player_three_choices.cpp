c781bf3562e70fbdc0b1292923e297d9ac0127e0
Artem Pushkin <idfumg@gmail.com>
2021-05-05 10:56:39 +0300

    Update dp/gfg/114_coin_game_winner_every_player_three_choices.cpp

--- dp/gfg/114_coin_game_winner_every_player_three_choices.cpp
+++ dp/gfg/114_coin_game_winner_every_player_three_choices.cpp
@@ -1,33 +1,39 @@
 #include "../../template.hpp"
 
-bool rec(int n, int x, int y, bool wasA) {
-    if (n == 0 and wasA) return true;
-    if (n <= 0) return false;
-    return
-        rec(n - 1, x, y, not wasA) or
-        rec(n - x, x, y, not wasA) or
-        rec(n - y, x, y, not wasA);
+int rec(int n, int x, int y, int isa) {
+    if (n == 0 and isa) return 1;
+    if (n <= 0) return 0;
+    return rec(n - 1, x, y, !isa) or rec(n - x, x, y, !isa) or rec(n - y, x, y, !isa);
 }
 
-char rec(int n, int x, int y) {
-    return rec(n, x, y, false) ? 'A' : 'B';
+int rec(int n, int x, int y) {
+    return rec(n - 1, x, y, 1) or rec(n - x, x, y, 1) or rec(n - y, x, y, 1);
 }
 
-char tab(int n, int x, int y) {
+int tab(int n, int x, int y) {
     vvi dp(n + 1, vi(2));
-    dp[0][0] = 1;
-    for (int i = 1; i <= n; ++i) {
-        for (int wasA : {0, 1}) {
-            dp[i][wasA] =
-                (i >= 1 ? dp[i - 1][!wasA] : 0) or
-                (i >= x ? dp[i - x][!wasA] : 0) or
-                (i >= y ? dp[i - y][!wasA] : 0);
+    for (int i = 0; i <= n; ++i) {
+        for (int j : {0, 1}) {
+            if (i == 0 and j == 1) {
+                dp[i][j] = 1;
+            }
+            else if (i == 0) {
+                dp[i][j] = 0;
+            }
+            else {
+                dp[i][j] = dp[i - 1][!j];
+                if (i >= x and not dp[i][j]) dp[i][j] = dp[i - x][!j];
+                if (i >= y and not dp[i][j]) dp[i][j] = dp[i - y][!j];
+            }
         }
     }
-    return dp[n][1] == 1 ? 'A' : 'B';
+    if (dp[n - 1][1]) return 1;
+    if (n >= x and dp[n - x][1]) return 1;
+    if (n >= y and dp[n - y][1]) return 1;
+    return 0;
 }
 
-int main() { TimeMeasure _; __x();
+int main() { // TimeMeasure _; __x();
     int n1 = 5, x1 = 3, y1 = 4;
     int n2 = 2, x2 = 3, y2 = 4;
     cout << rec(n1, x1, y1) << endl; // A
