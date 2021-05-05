4c6015983e530bfc7ec95a9371ac59ba8f2c3a7a
Artem Pushkin <idfumg@gmail.com>
2021-05-05 10:56:39 +0300

    Update greedy/gfg/016_maximum_sum_absolute_difference_array.cpp

--- greedy/gfg/016_maximum_sum_absolute_difference_array.cpp
+++ greedy/gfg/016_maximum_sum_absolute_difference_array.cpp
@@ -44,6 +44,23 @@ int MaximumSumOfDifferences2(vi arr) {
     return ans;
 }
 
+int MaximumSumOfDifferences3(vi arr) {
+    sort(arr.begin(), arr.end());
+    int n = arr.size();
+    int i = 0;
+    int j = n - 1;
+    int ans = 0;
+    while (i < j) {
+        ans += arr[j] - arr[i++];
+        if (i < j) {
+            ans += arr[j--] - arr[i];
+        }
+    }
+    if (i >= j) {
+        ans += abs(arr[i] - arr[0]);
+    }
+    return ans;
+}
 
 int main() { TimeMeasure _;
     vi arr1 = { 4, 2, 1, 8 };
@@ -60,4 +77,9 @@ int main() { TimeMeasure _;
     cout << MaximumSumOfDifferences2(arr2) << '\n'; // 30 | {1, 10, 2, 8, 4}
     cout << MaximumSumOfDifferences2(arr3) << '\n'; // 10 | {10, 15, 12}
     cout << MaximumSumOfDifferences2(arr4) << '\n'; // 26
+    cout << '\n';
+    cout << MaximumSumOfDifferences3(arr1) << '\n'; // 18 | {1, 8, 2, 4}
+    cout << MaximumSumOfDifferences3(arr2) << '\n'; // 30 | {1, 10, 2, 8, 4}
+    cout << MaximumSumOfDifferences3(arr3) << '\n'; // 10 | {10, 15, 12}
+    cout << MaximumSumOfDifferences3(arr4) << '\n'; // 26
 }
