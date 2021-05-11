b73c875f653f39678dadfb0bc8e6acbd65815951
Artem Pushkin <idfumg@gmail.com>
2021-05-11 21:58:09 +0300

    Update dp/gfg/143_remove_minimum_elements_either_side_2min_max.cpp

--- dp/gfg/143_remove_minimum_elements_either_side_2min_max.cpp
+++ dp/gfg/143_remove_minimum_elements_either_side_2min_max.cpp
@@ -1,83 +1,60 @@
 #include "../../template.hpp"
 
+vvi getMins(vi arr) {
+    int n = arr.size();
+    vvi dp(n + 2, vi(n + 1, INF));
+    for (int i = n; i >= 1; --i) {
+        for (int j = 1; j <= n; ++j) {
+            if (i > j) dp[i][j] = INF;
+            else if (i == j) dp[i][j] = arr[i - 1];
+            else if (j - i == 1) dp[i][j] = min(arr[i - 1], arr[j - 1]);
+            else dp[i][j] = min(arr[i - 1], dp[i + 1][j]);
+        }
+    }
+    return dp;
+}
+
+vvi getMaxs(vi arr) {
+    int n = arr.size();
+    vvi dp(n + 2, vi(n + 1, -INF));
+    for (int i = n; i >= 1; --i) {
+        for (int j = 1; j <= n; ++j) {
+            if (i > j) dp[i][j] = -INF;
+            else if (i == j) dp[i][j] = arr[i - 1];
+            else if (j - i == 1) dp[i][j] = max(arr[i - 1], arr[j - 1]);
+            else dp[i][j] = max(arr[i - 1], dp[i + 1][j]);
+        }
+    }
+    return dp;
+}
+
 int rec(vi arr, int i, int j) {
     if (i >= j) return 0;
-    int mini = *min_element(arr.begin() + i, arr.begin() + j + 1);
-    int maxi = *max_element(arr.begin() + i, arr.begin() + j + 1);
-    if (2 * mini > maxi) return 0;
+    vvi mins = getMins(arr);
+    vvi maxs = getMaxs(arr);
+    if (2 * mins[i][j] > maxs[i][j]) return 0;
     return min(rec(arr, i + 1, j), rec(arr, i, j - 1)) + 1;
 }
 
 int rec(vi arr) {
-    return rec(arr, 0, arr.size() - 1);
+    return rec(arr, 1, arr.size());
 }
 
 int tab(vi arr) {
     int n = arr.size();
     vvi dp(n + 1, vi(n + 1));
-    for (int i = n - 1; i >= 0; --i) {
-        for (int j = 0; j < n; ++j) {
-            if (i >= j) {
-                dp[i][j] = 0;
-            }
-            else {
-                int mini = *min_element(arr.begin() + i, arr.begin() + j + 1);
-                int maxi = *max_element(arr.begin() + i, arr.begin() + j + 1);
-                if (2 * mini > maxi) {
-                    dp[i][j] = 0;
-                }
-                else {
-                    dp[i][j] = min(dp[i + 1][j], dp[i][j - 1]) + 1;
-                }
-            }
+    vvi mins = getMins(arr);
+    vvi maxs = getMaxs(arr);
+    for (int i = n; i >= 1; --i) {
+        for (int j = 1; j <= n; ++j) {
+            if (2 * mins[i][j] > maxs[i][j]) dp[i][j] = 0;
+            else dp[i][j] = min(dp[i + 1][j], dp[i][j - 1]) + 1;
         }
     }
-    return dp[0][n - 1];
+    return dp[1][n];
 }
 
-int tab2(vi arr) {
-    int n = arr.size();
-    vvi mins(n + 2, vi(n + 1));
-    vvi maxs(n + 2, vi(n + 1));
-    for (int i = n - 1; i >= 0; --i) {
-        for (int j = 0; j < n; ++j) {
-            if (i > j) {
-                mins[i][j] = INF;
-            }
-            else if (i == j) {
-                mins[i][j] = arr[i];
-            }
-            else if (j - i == 1) {
-                mins[i][j] = min(arr[i], arr[j]);
-                maxs[i][j] = max(arr[i], arr[j]);
-            }
-            else {
-                mins[i][j] = min(arr[i], mins[i + 1][j]);
-                maxs[i][j] = max(arr[i], maxs[i + 1][j]);
-            }
-        }
-    }
-
-    vvi dp(n + 2, vi(n + 1));
-    for (int i = n - 1; i >= 0; --i) {
-        for (int j = 0; j < n; ++j) {
-            if (i >= j) {
-                dp[i][j] = 0;
-            }
-            else {
-                if (2 * mins[i][j] > maxs[i][j]) {
-                    dp[i][j] = 0;
-                }
-                else {
-                    dp[i][j] = min(dp[i + 1][j], dp[i][j - 1]) + 1;
-                }
-            }
-        }
-    }
-    return dp[0][n - 1];
-}
-
-int main() { // TimeMeasure _; __x();
+int main() { TimeMeasure _; __x();
     vi arr1 = {4, 5, 100, 9, 10, 11, 12, 15, 200}; // 4
     vi arr2 = {4, 7, 5, 6}; // 0
     vi arr3 = {20, 7, 5, 6}; // 1
@@ -92,9 +69,4 @@ int main() { // TimeMeasure _; __x();
     cout << tab(arr2) << endl;
     cout << tab(arr3) << endl;
     cout << tab(arr4) << endl;
-    cout << '\n';
-    cout << tab2(arr1) << endl;
-    cout << tab2(arr2) << endl;
-    cout << tab2(arr3) << endl;
-    cout << tab2(arr4) << endl;
 }
