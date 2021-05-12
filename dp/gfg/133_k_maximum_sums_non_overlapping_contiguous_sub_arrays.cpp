85a5b41198b293a1d9b3b90237cbfd783622c19b
Artem Pushkin <idfumg@gmail.com>
2021-05-12 21:47:10 +0300

    Update dp/gfg/133_k_maximum_sums_non_overlapping_contiguous_sub_arrays.cpp

--- dp/gfg/133_k_maximum_sums_non_overlapping_contiguous_sub_arrays.cpp
+++ dp/gfg/133_k_maximum_sums_non_overlapping_contiguous_sub_arrays.cpp
@@ -1,42 +1,36 @@
 #include "../../template.hpp"
 
-vi tab(vi arr, int K) {
+vi tab(vi arr, int k) {
     int n = arr.size();
-    vvi res;
-    vi sums;
-    for (int k = 1; k <= K; ++k) {
-        int maxi = -INF, maxipos = 0;
-        vi prev(n, -1), dp = arr;
+    vi ans;
+    while (k--) {
+        vi dp = arr;
+        vi prev(n, -1);
         for (int i = 1; i < n; ++i) {
-            if (dp[i - 1] + arr[i] > dp[i]) {
-                dp[i] = dp[i - 1] + arr[i];
+            if (arr[i] + dp[i - 1] > arr[i]) {
+                dp[i] = arr[i] + dp[i - 1];
                 prev[i] = i - 1;
             }
-            else {
-                dp[i] = arr[i];
-                prev[i] = -1;
-            }
-            if (dp[i] > maxi) {
-                maxi = dp[i];
-                maxipos = i;
+        }
+        int maxsum = -INF;
+        int maxidx = 0;
+        for (int i = 0; i < n; ++i) {
+            if (dp[i] > maxsum) {
+                maxsum = dp[i];
+                maxidx = i;
             }
         }
-        vi elems;
-        for (int at = maxipos; at != -1; at = prev[at]) {
-            elems.push_back(arr[at]);
+        ans.push_back(dp[maxidx]);
+        for (int at = maxidx; at != -1; at = prev[at]) {
             arr[at] = -INF;
         }
-        reverse(elems);
-        res.push_back(elems);
-        sums.push_back(maxi);
     }
-    debug(res);
-    return sums;
+    return ans;
 }
 
 int main() { TimeMeasure _; __x();
     vi arr1 = {4, 1, 1, -1, -3, -5, 6, 2, -6, -2}; int k1 = 3;
     vi arr2 = {5, 1, 2, -6, 2, -1, 3, 1}; int k2 = 2;
-    cout << tab(arr1, k1) << endl;
-    cout << tab(arr2, k2) << endl;
+    cout << tab(arr1, k1) << endl; // 8 6 -1
+    cout << tab(arr2, k2) << endl; // 8 5
 }
