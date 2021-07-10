75fe63eca058868b776615bdb80eef78b3378c0c
Artem Pushkin <idfumg@gmail.com>
2021-07-10 11:00:06 +0300

    Update dp/gfg/158_count_sum_of_digits_in_numbers_from_1_to_n.cpp

--- dp/gfg/158_count_sum_of_digits_in_numbers_from_1_to_n.cpp
+++ dp/gfg/158_count_sum_of_digits_in_numbers_from_1_to_n.cpp
@@ -11,20 +11,22 @@ int naive(int num) {
 }
 
 int rec_(int n) {
-    if (n < 10) return n;
-    return n % 10 + rec_(n / 10);
+    if (n == 0) return 0;
+    return rec_(n / 10) + n % 10;
 }
 
 int rec(int n) {
-    if (n == 0) return 0;
-    return rec_(n) + rec(n - 1);
+    int ans = 0;
+    for (int i = 1; i <= n; ++i) {
+        ans += rec_(i);
+    }
+    return ans;
 }
 
 int tab(int n) {
     vi dp(n + 1);
     for (int i = 1; i <= n; ++i) {
-        if (i < 10) dp[i] = i;
-        else dp[i] = i % 10 + dp[i / 10];
+        dp[i] = dp[i / 10] + i % 10;
     }
     int ans = 0;
     for (int i = 1; i <= n; ++i) {
@@ -33,13 +35,15 @@ int tab(int n) {
     return ans;
 }
 
-int main() { // TimeMeasure _; __x();
+int main() { TimeMeasure _; __x();
     cout << naive(5) << endl; // 15
     cout << naive(12) << endl; // 51
     cout << naive(328) << endl; // 3241
+    cout << endl;
     cout << rec(5) << endl; // 15
     cout << rec(12) << endl; // 51
     cout << rec(328) << endl; // 3241
+    cout << endl;
     cout << tab(5) << endl; // 15
     cout << tab(12) << endl; // 51
     cout << tab(328) << endl; // 3241
