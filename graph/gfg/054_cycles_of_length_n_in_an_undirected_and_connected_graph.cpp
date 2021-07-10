df9cf896f86e08aeac2571db03908e1e23175b36
Artem Pushkin <idfumg@gmail.com>
2021-07-10 11:01:05 +0300

    Update graph/gfg/054_cycles_of_length_n_in_an_undirected_and_connected_graph.cpp

--- graph/gfg/054_cycles_of_length_n_in_an_undirected_and_connected_graph.cpp
+++ graph/gfg/054_cycles_of_length_n_in_an_undirected_and_connected_graph.cpp
@@ -1,27 +1,41 @@
 #include "../../template.hpp"
 
-void dfs(vvb& graph, vb& visited, int len, int at, int& count, int root) {
-    int n = graph.size();
+int dfs(vvb graph, vb& visited, int root, int k, int parent, int at, vi& path, set<vi>& paths) {
+    if (k == 0 and at == root) {
+        paths.insert(path);
+        return 1;
+    }
+    if (k <= 0) return 0;
     visited[at] = true;
-    for (int i = 0; i < n; ++i) {
-        if (graph[at][i]) {
-            if (not visited[i]) {
-                dfs(graph, visited, len - 1, i, count, at);
-            }
-            else if (i == root and len - 1 == 0) {
-                ++count;
+    int n = graph.size();
+    int count = 0;
+    for (int adj = 0; adj < n; ++adj) {
+        if (adj == parent) continue;
+        if (graph[at][adj]) {
+            if (adj == root or not visited[adj]) {
+                path.push_back(adj);
+                count += dfs(graph, visited, root, k - 1, at, adj, path, paths);
+                path.pop_back();
             }
         }
     }
+    visited[at] = false;
+    return count;
 }
 
-void CountCycles(vvb& graph, int len) {
-    int n = graph.size(), count = 0;
-    for (int i = 0; i < n; ++i) {
-        vb visited(n);
-        dfs(graph, visited, len, i, count, i);
+void CountCycles(vvb graph, int k) {
+    int n = graph.size();
+    vb visited(n);
+    int count = 0;
+    set<vi> paths;
+    for (int at = 0; at < n; ++at) {
+        if (not visited[at]) {
+            vi path = {at};
+            count += dfs(graph, visited, at, k - 0, -1, at, path, paths);
+        }
     }
-    cout << count / 2 << endl;
+    debugn(paths);
+    debugn(count / 2);
 }
 
 int main() { TimeMeasure _;
@@ -30,5 +44,5 @@ int main() { TimeMeasure _;
                  {0, 1, 0, 1, 0},
                  {1, 0, 1, 0, 1},
                  {0, 1, 0, 1, 0}};
-    CountCycles(graph, 4);
+    CountCycles(graph, 4); // 12
 }
