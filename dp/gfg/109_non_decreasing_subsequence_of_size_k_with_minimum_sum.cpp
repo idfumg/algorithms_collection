d20963fc662b95ed448864014b225251d0f8f396
Artem Pushkin <idfumg@gmail.com>
2021-07-10 10:58:07 +0300

    Update dp/gfg/109_non_decreasing_subsequence_of_size_k_with_minimum_sum.cpp

--- dp/gfg/109_non_decreasing_subsequence_of_size_k_with_minimum_sum.cpp
+++ dp/gfg/109_non_decreasing_subsequence_of_size_k_with_minimum_sum.cpp
@@ -1,74 +1,62 @@
 #include "../../template.hpp"
 
-bool IsNonDecreasing(vi& arr) {
+int naive(vi arr, int k) {
     int n = arr.size();
-    for (int i = 1; i < n; ++i) {
-        if (arr[i] < arr[i - 1]) {
-            return false;
-        }
-    }
-    return true;
-}
-
-void naive(vi& arr, int k, int& res, vi& current, int from) {
-    int n = arr.size(), m = current.size();
-    if (m > k) {
-        return;
-    }
-    if (from == n) {
-        if (current.size() == k and IsNonDecreasing(current)) {
-            res = min(res, accumulate(current.begin(), current.end(), 0));
-        }
-        return;
-    }
-    for (int i = from; i < n; ++i) {
-        naive(arr, k, res, current, i + 1);
-        current.push_back(arr[i]);
-        naive(arr, k, res, current, i + 1);
-        current.pop_back();
-    }
-}
-
-int naive(vi& arr, int k) {
-    int res = INF;
-    vi current;
-    naive(arr, k, res, current, 0);
-    return res;
-}
-
-int tab(vi& arr, int K) {
-    int n = arr.size();
-    vvi dp(K + 1, vi(n + 1, INF));
-    for (int i = 1; i <= n; ++i) {
-        dp[1][i] = arr[i - 1];
-    }
-    for (int k = 2; k <= K; ++k) {
-        for (int i = 1; i <= n; ++i) {
-            for (int j = 0; j < i; ++j) {
-                if (arr[i - 1] > arr[j - 1]) {
-                    dp[k][i] = min(dp[k][i], dp[k - 1][j] + arr[i - 1]);
-                }
+    int ans = INF;
+    for (int i = 1; i < (1 << n); ++i) {
+        vi subseq;
+        for (int j = 0; j < n; ++j) {
+            if (i & (1 << j)) {
+                subseq.push_back(arr[j]);
             }
         }
+        if (is_sorted(subseq.begin(), subseq.end()) and subseq.size() == k) {
+            ans = min(ans, accumulate(subseq.begin(), subseq.end(), 0));
+        }
     }
-    return min(dp[K]);
+    return ans;
 }
 
 int rec(vi arr, int k, int n, int prev) {
     if (k == 0) return 0;
     if (n == 0) return INF;
-    if (arr[n - 1] <= arr[prev - 1]) {
-        return min(rec(arr, k - 1, n - 1, n) + arr[n - 1],
-                   rec(arr, k, n - 1, prev));
-    }
+    if (prev == INF) return min(rec(arr, k - 1, n - 1, n) + arr[n - 1], rec(arr, k, n - 1, prev));
+    if (arr[n - 1] <= arr[prev - 1]) return min(rec(arr, k - 1, n - 1, n) + arr[n - 1], rec(arr, k, n - 1, prev));
     return rec(arr, k, n - 1, prev);
 }
 
 int rec(vi arr, int k) {
-    int ans = INF;
+    return rec(arr, k, arr.size(), INF);
+}
+
+int tab(vi arr, int K) {
     int n = arr.size();
-    for (int i = 1; i <= n; ++i) {
-        ans = min(ans, rec(arr, k - 1, i - 1, i) + arr[i - 1]);
+    vvvi dp(K + 1, vvi(n + 1, vi(n + 1)));
+    for (int k = 0; k <= K; ++k) {
+        for (int i = 0; i <= n; ++i) {
+            for (int j = 0; j <= n; ++j) {
+                if (k == 0) dp[k][i][j] = 0;
+                else if (i == 0) dp[k][i][j] = INF;
+                else if (j == 0) dp[k][i][j] = min(dp[k - 1][i - 1][i] + arr[i - 1], dp[k][i - 1][j]);
+                else if (arr[i - 1] <= arr[j - 1]) dp[k][i][j] = min(dp[k - 1][i - 1][i] + arr[i - 1], dp[k][i - 1][j]);
+                else dp[k][i][j] = dp[k][i - 1][j];
+            }
+        }
+    }
+    return dp[K][n][n];
+}
+
+int rec2(vi arr, int k, int n, int prev) {
+    if (k == 0) return 0;
+    if (n == 0) return INF;
+    if (arr[n - 1] <= arr[prev - 1]) return min(rec2(arr, k - 1, n - 1, n) + arr[n - 1], rec2(arr, k, n - 1, prev));
+    return rec2(arr, k, n - 1, prev);
+}
+
+int rec2(vi arr, int k) {
+    int ans = INF;
+    for (int i = 1; i <= arr.size(); ++i) {
+        ans = min(ans, rec2(arr, k - 1, i - 1, i) + arr[i - 1]);
     }
     return ans;
 }
@@ -78,20 +66,11 @@ int tab2(vi arr, int K) {
     vvvi dp(K + 1, vvi(n + 1, vi(n + 1)));
     for (int k = 0; k <= K; ++k) {
         for (int i = 0; i <= n; ++i) {
-            for (int j = 0; j <= n; ++j) {
-                if (k == 0) {
-                    dp[k][i][j] = 0;
-                }
-                else if (i == 0) {
-                    dp[k][i][j] = INF;
-                }
-                else if (arr[i - 1] <= arr[j - 1]) {
-                    dp[k][i][j] = min((k > 0 ? dp[k - 1][i - 1][i] : 0) + arr[i - 1],
-                                      dp[k][i - 1][j]);
-                }
-                else {
-                    dp[k][i][j] = dp[k][i - 1][j];
-                }
+            for (int j = 1; j <= n; ++j) {
+                if (k == 0) dp[k][i][j] = 0;
+                else if (i == 0) dp[k][i][j] = INF;
+                else if (arr[i - 1] <= arr[j - 1]) dp[k][i][j] = min(dp[k - 1][i - 1][i] + arr[i - 1], dp[k][i - 1][j]);
+                else dp[k][i][j] = dp[k][i - 1][j];
             }
         }
     }
@@ -111,13 +90,17 @@ int main() { TimeMeasure _; __x();
     cout << naive(arr2, k2) << '\n'; // 120
     cout << naive(arr3, k3) << '\n'; // 206
     cout << '\n';
+    cout << rec(arr1, k1) << '\n'; // 39
+    cout << rec(arr2, k2) << '\n'; // 120
+    cout << rec(arr3, k3) << '\n'; // 206
+    cout << '\n';
     cout << tab(arr1, k1) << '\n'; // 39
     cout << tab(arr2, k2) << '\n'; // 120
     cout << tab(arr3, k3) << '\n'; // 206
     cout << '\n';
-    cout << rec(arr1, k1) << '\n'; // 39
-    cout << rec(arr2, k2) << '\n'; // 120
-    cout << rec(arr3, k3) << '\n'; // 206
+    cout << rec2(arr1, k1) << '\n'; // 39
+    cout << rec2(arr2, k2) << '\n'; // 120
+    cout << rec2(arr3, k3) << '\n'; // 206
     cout << '\n';
     cout << tab2(arr1, k1) << '\n'; // 39
     cout << tab2(arr2, k2) << '\n'; // 120
