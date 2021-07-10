f3d5616723ebd5d5ffe73d98b236241529fad45a
Artem Pushkin <idfumg@gmail.com>
2021-07-10 10:58:16 +0300

    Update dp/gfg/114_coin_game_winner_every_player_three_choices.cpp

--- dp/gfg/114_coin_game_winner_every_player_three_choices.cpp
+++ dp/gfg/114_coin_game_winner_every_player_three_choices.cpp
@@ -1,43 +1,58 @@
 #include "../../template.hpp"
 
-int rec(int n, int x, int y, int isa) {
-    if (n == 0 and isa) return 1;
-    if (n <= 0) return 0;
-    return rec(n - 1, x, y, !isa) or rec(n - x, x, y, !isa) or rec(n - y, x, y, !isa);
+bool rec(int n, int x, int y, int isa) {
+    if (n == 0 and isa) return true;
+    else if (n <= 0) return false;
+    for (int coins : {1, x, y}) {
+        if (rec(n - coins, x, y, !isa)) {
+            return true;
+        }
+    }
+    return false;
 }
 
-int rec(int n, int x, int y) {
-    return rec(n - 1, x, y, 1) or rec(n - x, x, y, 1) or rec(n - y, x, y, 1);
+char rec(int n, int x, int y) {
+    for (int coins : {1, x, y}) {
+        if (rec(n - coins, x, y, true)) {
+            return 'A';
+        }
+    }
+    return 'B';
 }
 
-int tab(int n, int x, int y) {
+char tab(int n, int x, int y) {
     vvi dp(n + 1, vi(2));
     for (int i = 0; i <= n; ++i) {
-        for (int j : {0, 1}) {
-            if (i == 0 and j == 1) {
-                dp[i][j] = 1;
+        for (int isa : {0, 1}) {
+            if (i == 0 and isa) {
+                dp[i][isa] = 1;
             }
             else if (i == 0) {
-                dp[i][j] = 0;
+                dp[i][isa] = 0;
             }
             else {
-                dp[i][j] = dp[i - 1][!j];
-                if (i >= x and not dp[i][j]) dp[i][j] = dp[i - x][!j];
-                if (i >= y and not dp[i][j]) dp[i][j] = dp[i - y][!j];
+                for (int coins : {1, x, y}) {
+                    if (i >= coins and dp[i - coins][!isa]) {
+                        dp[i][isa] = 1;
+                    }
+                }
             }
         }
     }
-    if (dp[n - 1][1]) return 1;
-    if (n >= x and dp[n - x][1]) return 1;
-    if (n >= y and dp[n - y][1]) return 1;
-    return 0;
+    for (int coins : {1, x, y}) {
+        if (n >= coins and dp[n - coins][1]) {
+            return 'A';
+        }
+    }
+    return 'B';
 }
 
-int main() { // TimeMeasure _; __x();
+int main() { TimeMeasure _; __x();
     int n1 = 5, x1 = 3, y1 = 4;
     int n2 = 2, x2 = 3, y2 = 4;
     cout << rec(n1, x1, y1) << endl; // A
     cout << rec(n2, x2, y2) << endl; // B
+    cout << endl;
     cout << tab(n1, x1, y1) << endl; // A
     cout << tab(n2, x2, y2) << endl; // B
 }
