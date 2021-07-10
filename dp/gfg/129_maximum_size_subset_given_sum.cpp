67c5267d1b02bfd275791386aaddc6168abfb95e
Artem Pushkin <idfumg@gmail.com>
2021-07-10 10:59:07 +0300

    Update dp/gfg/129_maximum_size_subset_given_sum.cpp

--- dp/gfg/129_maximum_size_subset_given_sum.cpp
+++ dp/gfg/129_maximum_size_subset_given_sum.cpp
@@ -1,76 +1,28 @@
 #include "../../template.hpp"
 
-int rec(vi arr, int sum, int n, int count) {
-    if (sum == 0) {
-        return count;
-    }
-    if (sum < 0 or n == 0) {
-        return 0;
-    }
-    return max(rec(arr, sum, n - 1, count),
-               rec(arr, sum - arr[n - 1], n - 1, count + 1));
+int rec(vi arr, int target, int n, int len) {
+    if (target == 0) return len;
+    if (n == 0 or target <= 0) return 0;
+    return max(rec(arr, target - arr[n - 1], n - 1, len + 1), rec(arr, target, n - 1, len));
 }
 
-int rec(vi arr, int sum) {
+int rec(vi arr, int target) {
+    return rec(arr, target, arr.size(), 0);
+}
+
+int tab(vi arr, int target) {
     int n = arr.size();
-    return rec(arr, sum, n, 0);
-}
-
-int rec2(vi arr, int sum, int n) {
-    if (sum <= 0 or n == 0) {
-        return 0;
-    }
-    return max(rec2(arr, sum, n - 1),
-               rec2(arr, sum - arr[n - 1], n - 1) + 1);
-}
-
-int rec2(vi arr, int sum) {
-    int n = arr.size();
-    return rec2(arr, sum, n);
-}
-
-int tab(vi arr, int sum) {
-    int n = arr.size();
-    vvvi dp(sum + 1, vvi(n + 1, vi(n + 2)));
-    for (int i = 0; i <= sum; ++i) {
-        for (int j = 0; j <= n; ++j) {
-            for (int k = n; k >= 0; --k) {
-                if (i == 0) {
-                    dp[i][j][k] = k;
-                }
-                else if (j == 0) {
-                    dp[i][j][k] = 0;
-                }
-                else if (i >= arr[j - 1]) {
-                    dp[i][j][k] = max(dp[i][j - 1][k], dp[i - arr[j - 1]][j - 1][k + 1]);
-                }
-                else {
-                    dp[i][j][k] = dp[i][j - 1][k];
-                }
-            }
-        }
-    }
-    return dp[sum][n][0];
-}
-
-int tab2(vi arr, int target) {
-    int n = arr.size();
-    vvi dp(target + 1, vi(n + 1));
+    vvvi dp(target + 1, vvi(n + 1, vi(n + 1)));
     for (int i = 0; i <= target; ++i) {
         for (int j = 0; j <= n; ++j) {
-            if (i == 0) {
-                dp[i][j] = 0;
-            }
-            else if (j == 0) {
-                dp[i][j] = -INF;
-            }
-            else {
-                dp[i][j] = dp[i][j - 1];
-                if (i >= arr[j - 1]) dp[i][j] = max(dp[i][j], dp[i - arr[j - 1]][j - 1] + 1);
+            for (int k = 0; k <= n; ++k) {
+                if (i == 0) dp[i][j][k] = k;
+                else if (j == 0) dp[i][j][k] = 0;
+                else dp[i][j][k] = max(dp[i][j - 1][k], i >= arr[j - 1] ? dp[i - arr[j - 1]][j - 1][k + 1] : 0);
             }
         }
     }
-    return dp[target][n];
+    return dp[target][n][0];
 }
 
 int main() { TimeMeasure _; __x();
@@ -82,15 +34,7 @@ int main() { TimeMeasure _; __x();
     cout << rec(arr2, sum2) << endl;
     cout << rec(arr3, sum3) << endl;
     cout << '\n';
-    cout << rec2(arr1, sum1) << endl;
-    cout << rec2(arr2, sum2) << endl;
-    cout << rec2(arr3, sum3) << endl;
-    cout << '\n';
     cout << tab(arr1, sum1) << endl;
     cout << tab(arr2, sum2) << endl;
     cout << tab(arr3, sum3) << endl;
-    cout << '\n';
-    cout << tab2(arr1, sum1) << endl;
-    cout << tab2(arr2, sum2) << endl;
-    cout << tab2(arr3, sum3) << endl;
 }
