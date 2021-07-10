5fb17a6bdff3c92b811369bf2df6115e46bf0d1f
Artem Pushkin <idfumg@gmail.com>
2021-07-10 10:58:46 +0300

    Update dp/gfg/119_count_number_binary_strings_without_consecutive_1s.cpp

--- dp/gfg/119_count_number_binary_strings_without_consecutive_1s.cpp
+++ dp/gfg/119_count_number_binary_strings_without_consecutive_1s.cpp
@@ -12,10 +12,10 @@ int rec(int n) {
 
 int tab(int n) {
     vvi dp(n + 1, vi(2));
-    dp[0][0] = dp[0][1] = 1;
-    for (int i = 1; i <= n; ++i) {
+    for (int i = 0; i <= n; ++i) {
         for (int j = 0; j <= 1; ++j) {
-            if (j == 0) dp[i][j] = dp[i - 1][0] + dp[i - 1][1];
+            if (i == 0) dp[i][j] = 1;
+            else if (j == 0) dp[i][j] = dp[i - 1][0] + dp[i - 1][1];
             else dp[i][j] = dp[i - 1][0];
         }
     }
@@ -27,38 +27,53 @@ int opt(int n) {
     int idx = 0;
     for (int i = 0; i <= n; ++i) {
         idx = i & 1;
-        for (int j : {0, 1}) {
+        for (int j = 0; j <= 1; ++j) {
             if (i == 0) dp[idx][j] = 1;
             else if (j == 0) dp[idx][j] = dp[1 - idx][0] + dp[1 - idx][1];
             else dp[idx][j] = dp[1 - idx][0];
         }
     }
-    return dp[idx][0];
+    return dp[1 - idx][0] + dp[1 - idx][1];
 }
 
-int with0(int n);
+int with1(int n);
 
+int with0(int n) {
+    if (n == 0) return 1;
+    return with0(n - 1) + with1(n - 1);
+}
 int with1(int n) {
-    if (n == 1) return 1;
+    if (n == 0) return 1;
     return with0(n - 1);
 }
 
-int with0(int n) {
-    if (n == 1) return 1;
+int rec2(int n) {
     return with0(n - 1) + with1(n - 1);
 }
 
-int rec1d(int n) {
-    return with0(n) + with1(n);
+int tab2(int n) {
+    vi w0(n + 1), w1(n + 1);
+    w0[0] = w1[0] = 1;
+    for (int i = 1; i < n; ++i) {
+        w1[i] = w0[i - 1];
+        w0[i] = w0[i - 1] + w1[i - 1];
+    }
+    return w0[n - 1] + w1[n - 1];
 }
 
-int main() { // TimeMeasure _; __x();
+int main() { TimeMeasure _; __x();
     cout << rec(2) << endl; // 3
     cout << rec(3) << endl; // 5
+    cout << endl;
     cout << tab(2) << endl;
     cout << tab(3) << endl;
+    cout << endl;
     cout << opt(2) << endl;
     cout << opt(3) << endl;
-    cout << rec1d(2) << endl;
-    cout << rec1d(3) << endl;
+    cout << endl;
+    cout << rec2(2) << endl;
+    cout << rec2(3) << endl;
+    cout << endl;
+    cout << tab2(2) << endl;
+    cout << tab2(3) << endl;
 }
